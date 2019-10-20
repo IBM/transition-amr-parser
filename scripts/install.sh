5a14565373d9e2c5aff47756d3cfb32ea0e0e7d8
ramon-astudillo <ramon@astudillo.com>
2019-10-19 21:36:16 -0400

    Update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,24 @@
 set -o errexit
 set -o pipefail 
+# Allow for extenal virtualenv for install as dependent module
+if [ -z "$1" ];then
+    virtualenv_name=venv
+else
+    virtualenv_name=$1
+fi
 set -o nounset
 
 # this assumes python 3.6+ available
 
 # modules
-pip install virtualenv --upgrade
-virtualenv venv
-. venv/bin/activate
-pip install -r requirements.txt
+if [ ! -d $virtualenv_name ];then
+    pip install virtualenv --upgrade
+    virtualenv  $virtualenv_name
+    .  ${virtualenv_name}/bin/activate
+    pip install -r requirements.txt
+else
+    echo "Will use existing $virtualenv_name, removet to force re-install"
+fi
 
 # spacy lemmatization
 python -m spacy download en
