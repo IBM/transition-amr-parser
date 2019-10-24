0d21b5b43ceb8ef15bff099df42511e6e7e6f25f
ramon-astudillo <ramon@astudillo.com>
2019-10-24 13:33:16 -0400

    WIP Minor refactoring
    - WIP move most of io to its own module
    - WIP cleaner parse in preparation for non-oracle execution

---
+++ transition_amr_parser/io.py
@@ -0,0 +1,70 @@
+import re
+import json
+from collections import Counter
+
+
+def read_rule_stats(rule_stats_json):
+
+    with open(rule_stats_json) as fid:
+        rule_stats = json.loads(fid.read())
+        # Fix counter
+        for rule in ['sense_by_token', 'lemma_by_token']:
+            rule_stats[rule] = {
+                key: Counter(value)
+                for key, value in rule_stats[rule].items()
+            }
+
+    return rule_stats
+
+
+def read_propbank(propbank_file):
+
+    # Read frame argument description
+    arguments_by_sense = {}
+    with open(propbank_file) as fid:
+        for line in fid:
+            line = line.rstrip()
+            sense = line.split()[0]
+            arguments = [
+                re.match('^(ARG.+):$', x).groups()[0]
+                for x in line.split()[1:] if re.match('^(ARG.+):$', x)
+            ]
+            arguments_by_sense[sense] = arguments
+
+    return arguments_by_sense
+
+
+def writer(file_path, add_return=False):
+    """
+    Returns a writer that writes to file_path if it is not None, does nothing
+    otherwise
+
+    calling the writed without arguments will close the file
+    """
+    if file_path:
+        # Erase file
+        fid = open(file_path, 'w+')
+        fid.close()
+        # open for appending
+        fid = open(file_path, 'a+', encoding='utf8')
+    else:
+        fid = None
+
+    def append_data(content=None):
+        """writes to open file"""
+        if fid:
+            if content is None:
+                fid.close()
+            else:
+                if add_return:
+                    fid.write(content + '\n')
+                else:
+                    fid.write(content)
+
+    return append_data
+
+
+def token_reader(file_path):
+    with open(file_path) as fid:
+        for line in fid:
+            yield line.rstrip().split()
