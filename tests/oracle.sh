b5267613b13a5ec7cda80b3ac94d62573400b454
ramon-astudillo <ramon@astudillo.com>
2019-10-30 21:10:45 -0400

    Update tests/oracle.sh

--- tests/oracle.sh
+++ tests/oracle.sh
@@ -9,34 +9,37 @@ set -o errexit
 oracle_folder=data/austin0_copy_literal/
 [ ! -d ${oracle_folder}/ ] && mkdir ${oracle_folder}/
 
-# # create oracle data
-# amr-oracle \
-#     --in-amr $train_file \
-#     --out-sentences ${oracle_folder}/train.tokens \
-#     --out-actions ${oracle_folder}/train.actions \
-#     --no-whitespace-in-actions \
-#     --out-rule-stats ${oracle_folder}/train.rules.json \
-#     #--out-amr ${oracle_folder}/train.oracle.amr \
-# 
-# # parse a sentence step by step
-# amr-parse \
-#     --in-sentences ${oracle_folder}/train.tokens \
-#     --in-actions ${oracle_folder}/train.actions \
-#     --out-amr ${oracle_folder}/train.amr \
-# 
-# # evaluate oracle performance
-# # wrt train.oracle.amr F-score: 0.9379
-# # wrt train.amr F-score: 0.9371
-# test_result="$(python smatch/smatch.py --significant 4 -f $train_file ${oracle_folder}/train.amr -r 10)"
-# echo $test_result
-# if [ "$test_result" != "F-score: 0.9371" ];then
-#     echo "Oracle train test failed! train F-score not 0.9371"
-#     exit 1
-# else
-#     echo "Oracle train test passed!"
-# fi
+# create oracle data
+amr-oracle \
+    --in-amr $train_file \
+    --out-sentences ${oracle_folder}/train.tokens \
+    --out-actions ${oracle_folder}/train.actions \
+    --no-whitespace-in-actions \
+    --out-rule-stats ${oracle_folder}/train.rules.json \
+    #--out-amr ${oracle_folder}/train.oracle.amr \
+
+# parse a sentence step by step
+amr-parse \
+    --in-sentences ${oracle_folder}/train.tokens \
+    --in-actions ${oracle_folder}/train.actions \
+    --out-amr ${oracle_folder}/train.amr \
+
+# evaluate oracle performance
+# wrt train.oracle.amr F-score: 0.9379
+# wrt train.amr F-score: 0.9371
+test_result="$(python smatch/smatch.py --significant 4 -f $train_file ${oracle_folder}/train.amr -r 10)"
+echo $test_result
+if [ "$test_result" != "F-score: 0.9371" ];then
+    echo "Oracle train test failed! train F-score not 0.9371"
+    exit 1
+else
+    echo "Oracle train test passed!"
+fi
 
 # DEV
+
+# ATTENTION: To pass the tests the dev test must have alignments as those
+# obatined with the preprocessing described in README
 
 # create oracle data
 echo "Generating Oracle"
