53567bf1d2317a65bdb71a4d9c797f42314d0f46
ramon-astudillo <ramon@astudillo.com>
2019-11-23 22:44:29 -0500

    Update oracle tests

--- tests/oracle.sh
+++ tests/oracle.sh
@@ -31,7 +31,7 @@ amr-parse \
 # F-score: valid actions + possible predicted 0.9365
 test_result="$(python smatch/smatch.py --significant 4 -f $train_file ${oracle_folder}/train.amr -r 10)"
 echo $test_result
-ref_score=0.9371
+ref_score=0.9366
 if [ "$test_result" != "F-score: $ref_score" ];then
     printf "[\033[91mFAILED\033[0m] Oracle train F-score not $ref_score\n"
     exit 1
