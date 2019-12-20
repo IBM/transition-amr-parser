3237b384416952040319308651bc4db218c91c0b
ramon-astudillo <ramon@astudillo.com>
2019-12-20 16:29:34 -0500

    WIP install faieseq

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,16 +4,14 @@ set -o nounset
 
 pip install --editable .
 
-# spacy lemmatization
-python -m spacy download en
+# Install fairseq
+[ -d fairseq ] && echo "fairseq/ exists, remove to reinstall" && exit 1
+git clone git@github.ibm.com:ramon-astudillo/fairseq.git
+cd fairseq
+pip install --editable .
 
 # Make smatch importable and editable, use MNLPs smatch (importable and faster) 
 rm -Rf smatch
 git clone git@github.ibm.com:mnlp/smatch.git
 # editable just in case
 pip install --editable smatch/
-
-# detailed smatch (wil need python2)
-# git clone https://github.com/mdtux89/amr-evaluation
-# cd amr-evaluation
-# pyenv local pypy2.7-7.0.0
