7b4b53d001cf8ed554aaba83fa5e3e438eda2930
ramon-astudillo <ramon@astudillo.com>
2020-01-06 14:00:24 -0500

    Update install x86

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,10 +5,9 @@ set -o nounset
 pip install --editable .
 
 # Install fairseq
-[ -d fairseq ] && echo "fairseq/ exists, remove to reinstall" && exit 1
-git clone git@github.ibm.com:ramon-astudillo/fairseq.git
-cd fairseq
-pip install --editable .
+[ ! -d fairseq ] && git clone git@github.ibm.com:ramon-astudillo/fairseq.git
+pip install -r fairseq/requirements.txt
+pip install --editable fairseq/
 
 # Make smatch importable and editable, use MNLPs smatch (importable and faster) 
 rm -Rf smatch
