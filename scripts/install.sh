fb0f29a95b230d1db2a186e9cf25ceee39f26005
ramon-astudillo <ramon@astudillo.com>
2020-01-10 06:26:52 -0500

    WIP simpler x86 installer

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,15 +2,11 @@ set -o errexit
 set -o pipefail 
 set -o nounset
 
+# See README for instructions on how to define this. You can comment this if
+# you are ok with instal on your active python version
+. set_environment.sh
+
+# Install requirements nad main module by separate. This is needed for
+# compatibility with PPC installer
+pip install -r requirements.txt
 pip install --editable .
-
-# Install fairseq
-[ ! -d fairseq ] && git clone git@github.ibm.com:ramon-astudillo/fairseq.git
-pip install -r fairseq/requirements.txt
-pip install --editable fairseq/
-
-# Make smatch importable and editable, use MNLPs smatch (importable and faster) 
-rm -Rf smatch
-git clone git@github.ibm.com:mnlp/smatch.git
-# editable just in case
-pip install --editable smatch/
