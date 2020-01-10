170266d5e6f8a9f195e48c2e7b50c519f437323a
ramon-astudillo <ramon@astudillo.com>
2020-01-10 12:40:50 -0500

    Add installer with no fairseq

--- scripts/install_no_fairseq.sh
+++ scripts/install_no_fairseq.sh
@@ -6,11 +6,6 @@ set -o nounset
 # you are ok with instal on your active python version
 . set_environment.sh
 
-# fairseq
-[ ! -d fairseq ] && git clone git@github.ibm.com:ramon-astudillo/fairseq.git
-pip install -r fairseq/requirements.txt
-pip install --editable fairseq/
-
 # Install requirements nad main module by separate. This is needed for
 # compatibility with PPC installer
 pip install -r requirements.txt
