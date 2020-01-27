d37a3bbadd7d5d99ee2ea6076c0d4069eb796eae
ramon-astudillo <ramon@astudillo.com>
2020-01-27 16:21:44 -0500

    Fix installer scripts for x86 and PPC

--- scripts/install_ppc.sh
+++ scripts/install_ppc.sh
@@ -1,36 +1,30 @@
 set -o errexit
 set -o pipefail 
-#set -o nounset 
+# See README for instructions on how to define this. You can comment this if
+# you are ok with instal on your active python version
+. set_environment.sh
+set -o nounset 
 
-[ -z "$CONDA_PREFIX" ] && echo "Expecting environment variable CONDA_PREFIX" && exit 1
+# use this environment for debugging (comment line above)
+# eval "$(${CONDA_DIR}/bin/conda shell.bash hook)"
+# rm -Rf ./tmp_debug
+# conda create -y -p ./tmp_debug
+# conda activate ./tmp_debug
 
-# We need to create an individual environment
-eval "$($CONDA_PREFIX/bin/conda shell.bash hook)"
-env_name=$(basename $(pwd))
-# Remove
-conda env remove -n $env_name
-# normal env
-
+# fairseq
+[ ! -d fairseq ] && git clone git@github.ibm.com:ramon-astudillo/fairseq.git
 # FIXME: Installing apex leads to FusedAdam error (missing parameter).
 # Commented for now.
 sed 's@ *- apex@# &@' -i fairseq/dcc/ccc_pcc_fairseq.yml
-
-[ ! -d fairseq ] && git clone git@github.ibm.com:ramon-astudillo/fairseq.git
-conda env create -n $env_name -f fairseq/dcc/ccc_pcc_fairseq.yml 
-conda activate $env_name
-
-# install transition_amr_parser
-sed 's@^@# @' -i requirements.txt
-pip install --editable .
-git checkout requirements.txt
-
-# install fairseq
+conda env update -f fairseq/dcc/ccc_pcc_fairseq.yml
 pip install --editable fairseq/
 
-# install pytorch scatter
-[ ! -d pytorch_scatter ] && git clone https://github.com/rusty1s/pytorch_scatter.git
+# pytorch scatter
+rm -Rf  pytorch_scatter
+git clone https://github.com/rusty1s/pytorch_scatter.git
 cd pytorch_scatter
-conda install -y pytest-runner -c powerai
+git checkout 1.3.2
+#conda install -y pytest-runner -c powerai
 # Ensure modern GCC
 export GCC_DIR=/opt/share/gcc-5.4.0/ppc64le/
 export PATH=/opt/share/cuda-9.0/ppc64le/bin:$GCC_DIR/bin:$PATH
@@ -39,8 +33,16 @@ export LD_LIBRARY_PATH=$GCC_DIR/lib64:$LD_LIBRARY_PATH
 python setup.py develop
 cd ..
 
-# Install smatch, use MNLPs smatch (importable and faster) 
-rm -Rf smatch
-git clone git@github.ibm.com:mnlp/smatch.git
-# editable just in case
-pip install --editable smatch/
+# this repo without the dependencies (included in fairseq/dcc/ccc_pcc_fairseq.yml)
+sed '/install_requires=install_requires,/d' -i setup.py
+pip install --editable . 
+# smatch
+[ ! -d smatch ] && git clone git@github.ibm.com:mnlp/smatch.git
+cd smatch
+git checkout f728c3d3f4a71b44678224d6934c1e67c4d37b89
+cd ..
+pip install smatch/
+
+# for debugging
+# conda install -y line_profiler
+# pip install ipdb
