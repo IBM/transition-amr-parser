45292208a66b44d8601602ae9bf7d6f0e996aca4
ramon-astudillo <ramon@astudillo.com>
2020-01-30 12:55:43 -0500

    Update all stack-transformer scripts
    better handling of variables. Global variables in config now are
    capitalized

--- scripts/stack-transformer/preprocess.sh
+++ scripts/stack-transformer/preprocess.sh
@@ -4,50 +4,42 @@ set -o pipefail
 . set_environment.sh
 set -o nounset 
 
-# load config
+# Argument handling
 config=$1
+
+# Load config
 . "$config"
 
 # stage-1: Preprocess
 
 # ORACLE
-[ ! -d $oracle_folder ] && mkdir -p $oracle_folder
-
-# Labeled shift: each time we shift, we also predict the word being shited
-# use top MAX_WORDS 
-# --multitask-max-words --out-multitask-words --in-multitask-words
-MAX_WORDS=100
-
-# To have an action calling external lemmatizer
-# --copy-lemma-action
+[ ! -d $ORACLE_FOLDER ] && mkdir -p $ORACLE_FOLDER
 
 # Create oracle data
+# Train
 amr-oracle \
-    --in-amr $amr_train_file \
-    --out-sentences $oracle_folder/train.en \
-    --out-actions $oracle_folder/train.actions \
-    --multitask-max-words $MAX_WORDS \
-    --out-multitask-words $oracle_folder/train.multitask_words \
-    --out-rule-stats $oracle_folder/train.rules.json \
-    --copy-lemma-action
+    --in-amr $AMR_TRAIN_FILE \
+    --out-sentences $ORACLE_FOLDER/train.en \
+    --out-actions $ORACLE_FOLDER/train.actions \
+    --out-rule-stats $ORACLE_FOLDER/train.rules.json \
+    $ORACLE_TRAIN_ARGS
+
+# Dev and test
+amr-oracle \
+    --in-amr $AMR_DEV_FILE \
+    --out-sentences $ORACLE_FOLDER/dev.en \
+    --out-actions $ORACLE_FOLDER/dev.actions \
+    --out-rule-stats $ORACLE_FOLDER/dev.rules.json \
+    $ORACLE_DEV_ARGS
 
 amr-oracle \
-    --in-amr $amr_dev_file \
-    --in-multitask-words $oracle_folder/train.multitask_words \
-    --out-sentences $oracle_folder/dev.en \
-    --out-actions $oracle_folder/dev.actions \
-    --out-rule-stats $oracle_folder/dev.rules.json \
-    --copy-lemma-action
-
-amr-oracle \
-    --in-amr $amr_test_file \
-    --in-multitask-words $oracle_folder/train.multitask_words \
-    --out-sentences $oracle_folder/test.en \
-    --out-actions $oracle_folder/test.actions \
-    --out-rule-stats $oracle_folder/test.rules.json \
-    --copy-lemma-action
+    --in-amr $AMR_TEST_FILE \
+    --out-sentences $ORACLE_FOLDER/test.en \
+    --out-actions $ORACLE_FOLDER/test.actions \
+    --out-rule-stats $ORACLE_FOLDER/test.rules.json \
+    $ORACLE_DEV_ARGS
 
 # PREPROCESSING
 # extract data
-echo "fairseq-preprocess $fairseq_preprocess_args"
-fairseq-preprocess $fairseq_preprocess_args 
+echo "fairseq-preprocess $FAIRSEQ_PREPROCESS_ARGS"
+fairseq-preprocess $FAIRSEQ_PREPROCESS_ARGS 
