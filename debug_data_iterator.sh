4343ca19eb213e13579b5b4d99f7060c74a146b6
ramon-astudillo <ramon@astudillo.com>
2020-02-10 17:29:11 -0500

    WIP Speed-up transformer training
    Contributes to https://github.ibm.com/mnlp/transition-amr-parser/issues/10
    
    27% reduction in epoch time through better memory initialization (see
    fairseq/ module)
    
    Changes are in fairseq/ see
    
    https://github.ibm.com/ramon-astudillo/fairseq/commit/d5ecfce02fa7ee047c8b4c59820e03bbf70bbc01

---
+++ debug_data_iterator.sh
@@ -0,0 +1,12 @@
+. set_environment.sh
+
+arguments="
+    DATA/amr/features/o3+Word100_RoBERTa-base/ 
+    --gen-subset train 
+    --batch-size 128
+"
+
+#python debug_data_iterator.py $arguments
+
+kernprof -l debug_data_iterator.py $arguments
+#python -m line_profiler debug_data_iterator.py.lprof
