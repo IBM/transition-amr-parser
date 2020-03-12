14f75146bd4f8e5cf0f5708aa47ad0d4796c9f21
ramon-astudillo <ramon@astudillo.com>
2020-03-12 19:45:58 -0400

    Add setting of CCC job name on cli

--- scripts/stack-transformer/jbsub_experiment.sh
+++ scripts/stack-transformer/jbsub_experiment.sh
@@ -2,6 +2,13 @@ set -o errexit
 set -o pipefail
 [ -z "$1" ] && echo -e "\ne.g. bash $0 /path/to/config.sh\n" && exit 1
 config=$1
+if [ -z "$2" ];then
+    # identify experiment by the repository tag
+    repo_tag="$(basename $(pwd) | sed 's@.*\.@@')"
+else
+    # identify experiment by given tag
+    repo_tag=$2
+fi
 set -o nounset
 
 # load config 
@@ -26,9 +33,6 @@ for index in $(seq $NUM_SEEDS);do
     cp $config $checkpoints_dir/config.sh
 
 done
-
-# identify the experiment by the repository tag
-repo_tag="$(basename $(pwd) | sed 's@.*\.@@')"
 
 # preprocessing
 echo "stage-1: Preprocess"
