c2812b96e02977956f2737d11c77d7a9ec25e1c0
ramon-astudillo <ramon@astudillo.com>
2020-03-12 13:03:38 -0400

    WIP set-up QALD config

--- scripts/stack-transformer/configs/QALD.sh
+++ scripts/stack-transformer/configs/QALD.sh
@@ -6,8 +6,8 @@ set -o nounset
 TASK_TAG=AMR
 
 # Global paths
-AMR_CORPORA=/dccstor/ykt-parse/SHARED/CORPORA/
-AMR_MODELS=/dccstor/ykt-parse/SHARED/MODELS/transition-amr-parser/
+AMR_CORPORA=/dccstor/ykt-parse/SHARED/CORPORA/AMR/
+AMR_MODELS=/dccstor/ykt-parse/SHARED/MODELS/AMR/transition-amr-parser/
 
 # All data stored here
 data_root=DATA/$TASK_TAG/
@@ -16,7 +16,7 @@ data_root=DATA/$TASK_TAG/
 # See transition_amr_parser/data_oracle.py:argument_parser
 AMR_TRAIN_FILE=${AMR_CORPORA}/QB20200113/qb.jkaln
 AMR_TEST_FILE=${AMR_CORPORA}/LDC2016T10_preprocessed_tahira/dev.txt.removedWiki.noempty.JAMRaligned 
-AMR_DEV_FILE=${AMR_CORPORA}/test.jkaln
+AMR_DEV_FILE=${AMR_CORPORA}/QB20200113/test.jkaln
 # Labeled shift: each time we shift, we also predict the word being shited
 # but restrict this to top MAX_WORDS. Controlled by
 # --multitask-max-words --out-multitask-words --in-multitask-words
@@ -35,19 +35,18 @@ ORACLE_DEV_ARGS="
     --copy-lemma-action
 "
 
-# FIXME: Maybe borow
 
 # PREPROCESSING
 # See fairseq/fairseq/options.py:add_preprocess_args
 PREPRO_TAG="RoBERTa-large-ysuklee-v1"
 # these wont be really used
 PREPRO_GPU_TYPE=v100
-PREPRO_QUEUE=x86_6h
+PREPRO_QUEUE=x86_24h
 features_folder=$data_root/features/${ORACLE_TAG}_${PREPRO_TAG}
 # TODO: Get this paths refred to the SHARED folder
-# /dccstor/ykt-parse/SHARED/MODELS/AMR/transition-amr-parser/features/qaldlarge_extracted/
-srcdict="/dccstor/ysuklee1/AMR/CodeBase/transition-amr-parser/fairseq/data-bin/LDCQALD_extracted/dict.en.txt"
-tgtdict="/dccstor/ysuklee1/AMR/CodeBase/transition-amr-parser/fairseq/data-bin/LDCQALD_extracted/dict.actions.txt"
+# ${AMR_MODELS}/features/qaldlarge_extracted/
+srcdict="${AMR_MODELS}/features/qaldlarge_extracted/dict.en.txt"
+tgtdict="${AMR_MODELS}/features/qaldlarge_extracted/dict.actions.txt"
 FAIRSEQ_PREPROCESS_ARGS="
     --source-lang en
     --target-lang actions
@@ -75,11 +74,12 @@ TRAIN_GPU_TYPE=v100
 TRAIN_QUEUE=ppc_24h
 # --lazy-load for very large corpora (data does not fit into RAM)
 # --bert-backprop do backprop though BERT
-# --save-dir is specified inside dcc/train.sh to account for the seed
+# NOTE: --save-dir is specified inside dcc/train.sh to account for the seed
+MAX_EPOCH=190
 CHECKPOINTS_DIR_ROOT="$data_root/models/${ORACLE_TAG}_${PREPRO_TAG}_${TRAIN_TAG}"
 # NOTE: We start from a pretrained model
-MAX_EPOCH=190
-pretrained="${AMR_MODELS}/models/stack_transformer_6x6_nopos-qaldlarge_prepro_o3+Word100-stnp6x6-seed42/checkpoint89.pt
+pretrained="${AMR_MODELS}/models/stack_transformer_6x6_nopos-qaldlarge_prepro_o3+Word100-stnp6x6-seed42/checkpoint89.pt"
+FAIRSEQ_TRAIN_ARGS="
     $features_folder
     --restore-file $pretrained
     --max-epoch $MAX_EPOCH
@@ -100,8 +100,7 @@ pretrained="${AMR_MODELS}/models/stack_transformer_6x6_nopos-qaldlarge_prepro_o3
     --keep-last-epochs 100
     --max-tokens 3584
     --log-format json
-    $do_lazy_load 
-    $fp16
+    --fp16
 "
 
 # TESTING
