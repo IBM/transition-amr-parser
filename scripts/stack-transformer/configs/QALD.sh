ee169dec6d9ebe40e7bf24075a1dd87e87c5611e
ramon-astudillo <ramon@astudillo.com>
2020-03-25 13:57:37 -0400

    Updates to QALD config
    - Add empty wiki files
    - Use pretrained model obtained with QALD_prepro.sh config
    - switch to latest configuration (roberta.large-top8, tops arch)
    - Add --reset-optimizer (otherwise would not work with PPC)

--- scripts/stack-transformer/configs/QALD.sh
+++ scripts/stack-transformer/configs/QALD.sh
@@ -6,6 +6,17 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+# NOTE: Assumes you ran training with
+#
+# scripts/stack-transformer/configs/QALD_prepro.sh
+#
+# to get (see use below)
+#
+# DATA/AMR/features/ldcQB_o3+Word100_RoBERTa-large-top8/dict.en.txt
+# DATA/AMR/features/ldcQB_o3+Word100_RoBERTa-large-top8/dict.actions.txt
+# DATA/AMR/models/ldcQB_o3+Word100_RoBERTa-large-top8_stops6x6-seed44/checkpoint_best_SMATCH.pt
+#
+
 TASK_TAG=AMR
 
 # Global paths
@@ -20,13 +31,19 @@ data_root=DATA/$TASK_TAG/
 AMR_TRAIN_FILE=${AMR_CORPORA}/QB20200113/qb.jkaln
 AMR_TEST_FILE=${AMR_CORPORA}/LDC2016T10_preprocessed_tahira/dev.txt.removedWiki.noempty.JAMRaligned 
 AMR_DEV_FILE=${AMR_CORPORA}/QB20200113/test.jkaln
+# WIKI files
+WIKI_DEV=""
+AMR_DEV_FILE_WIKI=""
+WIKI_TEST=""
+AMR_TEST_FILE_WIKI=""
+
 # Labeled shift: each time we shift, we also predict the word being shited
 # but restrict this to top MAX_WORDS. Controlled by
 # --multitask-max-words --out-multitask-words --in-multitask-words
 # To have an action calling external lemmatizer (SpaCy)
 # --copy-lemma-action
 MAX_WORDS=100
-ORACLE_TAG=finetune_o3+Word${MAX_WORDS}
+ORACLE_TAG=qb_finetune_o3+Word${MAX_WORDS}
 ORACLE_FOLDER=$data_root/oracles/${ORACLE_TAG}/
 ORACLE_TRAIN_ARGS="
     --multitask-max-words $MAX_WORDS 
@@ -41,15 +58,15 @@ ORACLE_DEV_ARGS="
 
 # PREPROCESSING
 # See fairseq/fairseq/options.py:add_preprocess_args
-PREPRO_TAG="RoBERTa-large-ysuklee-v1"
-# these wont be really used
+PREPRO_TAG="RoBERTa-large-top8"
+# CCC configuration in scripts/stack-transformer/jbsub_experiment.sh
 PREPRO_GPU_TYPE=v100
 PREPRO_QUEUE=x86_24h
-features_folder=$data_root/features/${ORACLE_TAG}_${PREPRO_TAG}
+features_folder=$data_root/features/${ORACLE_TAG}_${PREPRO_TAG}/
 # TODO: Get this paths refred to the SHARED folder
 # ${AMR_MODELS}/features/qaldlarge_extracted/
-srcdict="${AMR_MODELS}/features/qaldlarge_extracted/dict.en.txt"
-tgtdict="${AMR_MODELS}/features/qaldlarge_extracted/dict.actions.txt"
+srcdict="DATA/AMR/features/ldcQB_o3+Word100_RoBERTa-large-top8/dict.en.txt"
+tgtdict="DATA/AMR/features/ldcQB_o3+Word100_RoBERTa-large-top8/dict.actions.txt"
 FAIRSEQ_PREPROCESS_ARGS="
     --source-lang en
     --target-lang actions
@@ -57,8 +74,9 @@ FAIRSEQ_PREPROCESS_ARGS="
     --validpref $ORACLE_FOLDER/dev
     --testpref $ORACLE_FOLDER/test
     --destdir $features_folder
-    --pretrained-embed roberta.large
     --workers 1 
+    --pretrained-embed roberta.large
+    --bert-layers 17 18 19 20 21 22 23 24
     --srcdict $srcdict
     --tgtdict $tgtdict
     --machine-type AMR 
@@ -68,11 +86,11 @@ FAIRSEQ_PREPROCESS_ARGS="
 
 # TRAINING
 # See fairseq/fairseq/options.py:add_optimization_args,add_checkpoint_args
-# model types defined in ./fairseq/models/transformer.py
-TRAIN_TAG=stnp6x6
-base_model=stack_transformer_6x6_nopos
+# model types defined in ./fairseq/fairseq/models/transformer.py
+TRAIN_TAG=stops6x6
+base_model=stack_transformer_6x6_tops_nopos
 # number of random seeds trained at once
-NUM_SEEDS=1
+NUM_SEEDS=3
 # CCC configuration in scripts/stack-transformer/jbsub_experiment.sh
 TRAIN_GPU_TYPE=v100
 TRAIN_QUEUE=ppc_24h
@@ -82,7 +100,7 @@ TRAIN_QUEUE=ppc_24h
 MAX_EPOCH=190
 CHECKPOINTS_DIR_ROOT="$data_root/models/${ORACLE_TAG}_${PREPRO_TAG}_${TRAIN_TAG}"
 # NOTE: We start from a pretrained model
-pretrained="${AMR_MODELS}/models/stack_transformer_6x6_nopos-qaldlarge_prepro_o3+Word100-stnp6x6-seed42/checkpoint89.pt"
+pretrained="DATA/AMR/models/ldcQB_o3+Word100_RoBERTa-large-top8_stops6x6-seed44/checkpoint_best_SMATCH.pt"
 FAIRSEQ_TRAIN_ARGS="
     $features_folder
     --restore-file $pretrained
@@ -104,9 +122,8 @@ FAIRSEQ_TRAIN_ARGS="
     --keep-last-epochs 100
     --max-tokens 3584
     --log-format json
+    --reset-optimizer
 "
-
-# --fp16
 
 # TESTING
 # See fairseq/fairseq/options.py:add_optimization_args,add_checkpoint_args
@@ -122,7 +139,7 @@ FAIRSEQ_GENERATE_ARGS="
     $features_folder 
     --gen-subset valid
     --machine-type AMR 
-    --machine-rules $ORACLE_FOLDER/train.rules.json \
+    --machine-rules $ORACLE_FOLDER/train.rules.json
     --beam ${beam_size}
     --batch-size 128
     --remove-bpe
