44e43cd0066d79d7d8547729d1d864948f60ee6c
ramon-astudillo <ramon@astudillo.com>
2020-04-02 14:17:35 -0400

    WIP Add dependency-parsing

--- scripts/stack-transformer/configs/PTB.sh
+++ scripts/stack-transformer/configs/PTB.sh
@@ -1,50 +1,21 @@
 # Set variables and environment for a give experiment
-#
-# Variables intended to be use outside of this script are CAPITALIZED
-#
 set -o errexit
 set -o pipefail
 set -o nounset
 
-TASK_TAG=AMR
+# Oracles are precomputed ans stored here
+PTB_ORACLE=/dccstor/ykt-parse/SHARED/MODELS/dep-parsing/transition-amr-parser/oracles/
+
+TASK_TAG=dep-parsing
 
 # All data stored here
 data_root=DATA/$TASK_TAG/
 
-LDC2016_AMR_CORPUS=/dccstor/ykt-parse/SHARED/CORPORA/AMR/LDC2016T10_preprocessed_tahira/
-
-# AMR ORACLE
-# See transition_amr_parser/data_oracle.py:argument_parser
-# NOTE: LDC2016_AMR_CORPUS should be defined in set_envinroment.sh
-AMR_TRAIN_FILE=$LDC2016_AMR_CORPUS/jkaln_2016_scr.txt 
-AMR_DEV_FILE=$LDC2016_AMR_CORPUS/dev.txt.removedWiki.noempty.JAMRaligned 
-AMR_TEST_FILE=$LDC2016_AMR_CORPUS/test.txt.removedWiki.noempty.JAMRaligned
-# WIKI files
-WIKI_DEV=/dccstor/multi-parse/amr/dev.wiki
-AMR_DEV_FILE_WIKI=/dccstor/ykt-parse/AMR/2016data/dev.txt 
-WIKI_TEST=/dccstor/multi-parse/amr/test.wiki
-AMR_TEST_FILE_WIKI=/dccstor/ykt-parse/AMR/2016data/test.txt
-
-# Labeled shift: each time we shift, we also predict the word being shited
-# but restrict this to top MAX_WORDS. Controlled by
-# --multitask-max-words --out-multitask-words --in-multitask-words
-# To have an action calling external lemmatizer (SpaCy)
-# --copy-lemma-action
-MAX_WORDS=100
-ORACLE_TAG=o3+Word${MAX_WORDS}
+# Dependency-parsing oracle
+# NOTE: This is precomputed
+# ORACLE_TAG=PTB_SD_3_3_0
+ORACLE_TAG=PTB_SD_3_3_0+Word100
 ORACLE_FOLDER=$data_root/oracles/${ORACLE_TAG}/
-ORACLE_TRAIN_ARGS="
-    --multitask-max-words $MAX_WORDS 
-    --out-multitask-words $ORACLE_FOLDER/train.multitask_words 
-    --copy-lemma-action
-"
-ORACLE_DEV_ARGS="
-    --in-multitask-words $ORACLE_FOLDER/train.multitask_words \
-    --copy-lemma-action
-"
-
-# GPU
-# k80, v100 (3 times faster)
 
 # PREPROCESSING
 # See fairseq/fairseq/options.py:add_preprocess_args
@@ -59,10 +30,10 @@ FAIRSEQ_PREPROCESS_ARGS="
     --trainpref $ORACLE_FOLDER/train
     --validpref $ORACLE_FOLDER/dev
     --testpref $ORACLE_FOLDER/test
-    --destdir $features_folder 
-    --workers 1 
-    --machine-type AMR
-    --machine-rules $ORACLE_FOLDER/train.rules.json 
+    --destdir $features_folder
+    --workers 1
+    --tokenize-by-whitespace
+    --machine-type $TASK_TAG
 "
 
 # TRAINING
@@ -78,7 +49,7 @@ TRAIN_QUEUE=ppc_24h
 # --lazy-load for very large corpora (data does not fit into RAM)
 # --bert-backprop do backprop though BERT
 # NOTE: --save-dir is specified inside dcc/train.sh to account for the seed
-MAX_EPOCH=110
+MAX_EPOCH=50
 CHECKPOINTS_DIR_ROOT="$data_root/models/${ORACLE_TAG}_${PREPRO_TAG}_${TRAIN_TAG}"
 FAIRSEQ_TRAIN_ARGS="
     $features_folder
@@ -97,7 +68,7 @@ FAIRSEQ_TRAIN_ARGS="
     --weight-decay 0.0
     --criterion label_smoothed_cross_entropy
     --label-smoothing 0.01
-    --keep-last-epochs 40
+    --keep-last-epochs 30
     --max-tokens 3584
     --log-format json
     --fp16
@@ -116,8 +87,7 @@ TEST_QUEUE=x86_6h
 FAIRSEQ_GENERATE_ARGS="
     $features_folder 
     --gen-subset valid
-    --machine-type AMR 
-    --machine-rules $ORACLE_FOLDER/train.rules.json
+    --machine-type $TASK_TAG
     --beam ${beam_size}
     --batch-size 128
     --remove-bpe
