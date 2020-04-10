999479d3a74140401d8954eed298ef9be4188da6
ramon-astudillo <ramon@astudillo.com>
2020-04-10 12:06:43 -0400

    Finish model import

--- scripts/stack-transformer/import_model.sh
+++ scripts/stack-transformer/import_model.sh
@@ -3,44 +3,59 @@ set -o pipefail
 # setup environment
 # . set_environment.sh
 # Argument handling
-model_folder=$1
-[ -z "$model_folder" ] && model_folder=""
+model_folder_name=$1
+[ -z "$model_folder_name" ] && model_folder_name=""
 set -o nounset 
 
 EXTERNAL_FOLDER=/dccstor/ykt-parse/SHARED/MODELS/AMR/transition-amr-parser/
+LOCAL_FOLDER=DATA/AMR/
 
-#
-[ "$model_folder" == "" ] && \
-    python scripts/stack-transformer/rank_model.py --checkpoints $EXTERNAL_FOLDER/models --seed-average
+# If model not provided, list available (seed averages)
+[ "$model_folder_name" == "" ] && \
+    python scripts/stack-transformer/rank_model.py \
+        --checkpoints $EXTERNAL_FOLDER/models && \
+    exit 1
+    # --seed-average && \
+
+# name of folder where checlpoints are
+model_folder=$EXTERNAL_FOLDER/models/$model_folder_name/
+
+# Load config
+[ ! -f "$model_folder/config.sh" ] && \
+    echo "Expected $model_folder/config.sh" && \
+    echo "Have you included e. g. -seed42 in the name?" && \
+    exit 1
+. "$model_folder/config.sh"
 
 # Check of model exists in remote
-target_oracle_folder="$EXTERNAL_FOLDER/oracles/${ORACLE_TAG}/"
-target_features_folder="$EXTERNAL_FOLDER/features/${ORACLE_TAG}_${PREPRO_TAG}/"
-target_checkpoints_dir_root="$EXTERNAL_FOLDER/models/$model_folder_name"
+source_oracle_folder="$EXTERNAL_FOLDER/oracles/${ORACLE_TAG}/"
+source_features_folder="$EXTERNAL_FOLDER/features/${ORACLE_TAG}_${PREPRO_TAG}/"
+source_checkpoints_dir_root="$EXTERNAL_FOLDER/models/$model_folder_name"
 
-# FIXME: We need this to be a global variable in configs. Right now DATA/AMR is
-# hardcoded here
-features_folder=DATA/AMR/features/${ORACLE_TAG}_${PREPRO_TAG}/
+target_oracle_folder="$LOCAL_FOLDER/oracles/${ORACLE_TAG}/"
+target_features_folder="$LOCAL_FOLDER/features/${ORACLE_TAG}_${PREPRO_TAG}/"
+target_checkpoints_dir_root="$LOCAL_FOLDER/models/$model_folder_name"
 
 # ORACLE
 if [ -d "$target_oracle_folder" ];then
     echo "Skiping existing: $target_oracle_folder"
 else
-    cp -R $ORACLE_FOLDER $EXTERNAL_FOLDER/oracles/
+    echo "ln -s $source_oracle_folder $LOCAL_FOLDER/oracles/"
+    ln -s $source_oracle_folder $LOCAL_FOLDER/oracles/
 fi
 
 # FEATURES
 if [ -d "$target_features_folder" ];then
     echo "Skiping existing: $target_features_folder"
 else
-    echo "cp -R $features_folder $EXTERNAL_FOLDER/features/"
-    cp -R $features_folder $EXTERNAL_FOLDER/features/
+    echo "ln -s $source_features_folder $LOCAL_FOLDER/features/"
+    ln -s $source_features_folder $LOCAL_FOLDER/features/
 fi
 
 # MODEL
 if [ -d "$target_checkpoints_dir_root" ];then
     echo "Skiping existing: $target_checkpoints_dir_root"
 else
-    echo "cp -R $model_folder $target_checkpoints_dir_root"
-    cp -R $model_folder $target_checkpoints_dir_root
+    echo "ln -s $source_checkpoints_dir_root $LOCAL_FOLDER/models/"
+    ln -s $source_checkpoints_dir_root $LOCAL_FOLDER/models/
 fi
