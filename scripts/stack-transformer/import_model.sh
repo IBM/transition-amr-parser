35409aaed0e163b6cfd1a56bcca6be8bb695d68e
ramon-astudillo <ramon@astudillo.com>
2020-04-10 17:18:56 -0400

    Add --copy-model flag to import_model.sh

--- scripts/stack-transformer/import_model.sh
+++ scripts/stack-transformer/import_model.sh
@@ -5,6 +5,13 @@ set -o pipefail
 # Argument handling
 model_folder_name=$1
 [ -z "$model_folder_name" ] && model_folder_name=""
+flag=$2
+if [ -z "$flag" ];then
+    flag=""
+elif [ "$flag" != "--copy-model" ];then
+    echo -e "\nbash $0 [<model_name> [--copy-model]]\n"
+    exit 1
+fi
 set -o nounset 
 
 EXTERNAL_FOLDER=/dccstor/ykt-parse/SHARED/MODELS/AMR/transition-amr-parser/
@@ -12,6 +19,7 @@ LOCAL_FOLDER=DATA/AMR/
 
 # If model not provided, list available (seed averages)
 [ "$model_folder_name" == "" ] && \
+    echo -e "\nModels available in $EXTERNAL_FOLDER" && \
     python scripts/stack-transformer/rank_model.py \
         --checkpoints $EXTERNAL_FOLDER/models && \
     exit 1
@@ -36,6 +44,11 @@ target_oracle_folder="$LOCAL_FOLDER/oracles/${ORACLE_TAG}/"
 target_features_folder="$LOCAL_FOLDER/features/${ORACLE_TAG}_${PREPRO_TAG}/"
 target_checkpoints_dir_root="$LOCAL_FOLDER/models/$model_folder_name"
 
+# Make folders if needed
+mkdir -p $LOCAL_FOLDER/oracles/
+mkdir -p $LOCAL_FOLDER/features/
+mkdir -p $LOCAL_FOLDER/models/
+
 # ORACLE
 if [ -d "$target_oracle_folder" ];then
     echo "Skiping existing: $target_oracle_folder"
@@ -56,6 +69,11 @@ fi
 if [ -d "$target_checkpoints_dir_root" ];then
     echo "Skiping existing: $target_checkpoints_dir_root"
 else
-    echo "ln -s $source_checkpoints_dir_root $LOCAL_FOLDER/models/"
-    ln -s $source_checkpoints_dir_root $LOCAL_FOLDER/models/
+    if [ "$flag" == "--copy-model" ];then
+        echo "cp -R $source_checkpoints_dir_root $LOCAL_FOLDER/models/"
+        cp -R $source_checkpoints_dir_root $LOCAL_FOLDER/models/
+    else
+        echo "ln -s $source_checkpoints_dir_root $LOCAL_FOLDER/models/"
+        ln -s $source_checkpoints_dir_root $LOCAL_FOLDER/models/
+    fi
 fi
