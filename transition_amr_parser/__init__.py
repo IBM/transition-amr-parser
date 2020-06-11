9cb5e6035c19bbd37eab7adc5fabd0851e5303c1
ramon-astudillo <ramon@astudillo.com>
2020-06-11 11:18:18 -0400

    breaking change: Add colater data utils
    this breaks the parser importability from the base module

--- transition_amr_parser/__init__.py
+++ transition_amr_parser/__init__.py
@@ -2,4 +2,4 @@
 # import os
 # root_path = os.path.dirname(os.path.realpath(f'{__file__}/../'))
 # ENTITY_RULES = f'{root_path}/data/entity_rules.json'
-from transition_amr_parser.amr_parser import AMRParser
+# from transition_amr_parser.amr_parser import AMRParser
