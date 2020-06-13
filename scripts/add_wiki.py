dcf25a1fcf5fab68b218361dd3d6f540415742a7
ramon-astudillo <ramon@astudillo.com>
2020-06-13 13:33:30 -0400

    Add missing add_wiki.py

---
+++ scripts/add_wiki.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+import operator
+import sys
+import re
+import string
+import os
+
+famr = open(sys.argv[1])
+fwiki = open(sys.argv[2])
+# FIXME: External dependency
+ftrn = open("/dccstor/multi-parse/amr/trn.wikis")
+
+wiki_ht = {}
+for line in ftrn:
+    if len(line.strip().split('\t')) != 2:
+        continue
+    (n,w) = line.strip().split('\t')
+    wiki_ht[n] = w
+
+all_wikis = []
+wikis = []
+for line in fwiki:
+    if line.strip() == "":
+        all_wikis.append(wikis)
+        wikis = []
+    else:
+        wikis.append(line.strip().split())
+
+lc = 0
+while True:
+    line = famr.readline()
+    if not line:
+        break
+    line = line.rstrip()
+    if line.strip()=="" :
+        lc += 1
+    if ":name" in line:
+
+        #get name
+        namelines = []
+        nextline = famr.readline()
+        namelines.append(nextline.rstrip())
+        tok = ""
+        while "op" in nextline:
+            tok += nextline[nextline.find("\"")+1:nextline.rfind("\"")] + " "
+            if ")" in nextline:
+                break
+            nextline = famr.readline()
+            namelines.append(nextline.rstrip())
+        tok = tok.strip()
+
+        #get wiki of the name
+        #print tok
+        if tok in wiki_ht:
+            wiki = wiki_ht[tok]
+            line = line.replace(":name",":wiki " + wiki + "\t:name")
+        else:
+            if tok != "":
+                for i in range(len(all_wikis[lc])):
+                    if tok.split()[0] in all_wikis[lc][i][0] :# or (all_wikis[lc][i][1] != '-' and all_wikis[lc][i][1] == tok) or tok in all_wikis[lc][i][0] :
+                        wiki = all_wikis[lc][i][1]
+                        if wiki != '-':
+                            wiki = "\""+wiki+"\""
+                        line = line.replace(":name",":wiki " + wiki + "\t:name")
+                        break
+
+        print(line)
+        print("\n".join(namelines))
+    else:
+        print(line)
--- tests/stack-transformer/overfit.sh
+++ tests/stack-transformer/overfit.sh
@@ -13,67 +13,68 @@ MODEL_FOLDER=DATA.tests/models/wiki25/
 RESULTS_FOLDER=$MODEL_FOLDER/beam1
 max_epoch=10
 
-# ORACLE EXTRACTION
-# Given sentence and aligned AMR, provide action sequence that generates the
-# AMR back
-mkdir -p $ORACLE_FOLDER
-amr-oracle \
-    --in-amr DATA/wiki25.jkaln \
-    --out-sentences $ORACLE_FOLDER/train.en \
-    --out-actions $ORACLE_FOLDER/train.actions \
-    --out-rule-stats $ORACLE_FOLDER/train.rules.json \
-    --copy-lemma-action
-# For this unit test train/dev/test is the same (memorize test)
-cp $ORACLE_FOLDER/train.en $ORACLE_FOLDER/dev.en
-cp $ORACLE_FOLDER/train.actions $ORACLE_FOLDER/dev.actions
-cp $ORACLE_FOLDER/train.en $ORACLE_FOLDER/test.en
-cp $ORACLE_FOLDER/train.actions $ORACLE_FOLDER/test.actions
-
-# PREPROCESSING
-# Extract sentence featrures and action sequence and store them in fairseq
-# format
-rm -Rf DATA.tests/features/wiki25/  # not as var for security
-mkdir -p $FEATURES_FOLDER
-fairseq-preprocess \
-    --source-lang en \
-    --target-lang actions \
-    --trainpref $ORACLE_FOLDER/train \
-    --validpref $ORACLE_FOLDER/dev \
-    --testpref $ORACLE_FOLDER/test \
-    --destdir $FEATURES_FOLDER \
-    --workers 1 \
-    --machine-type AMR \
-    --machine-rules $ORACLE_FOLDER/train.rules.json 
- 
-# TRAINING
-rm -Rf DATA.tests/models/wiki25/
-fairseq-train \
-    $FEATURES_FOLDER \
-    --max-epoch $max_epoch \
-    --arch stack_transformer_6x6_tops_nopos \
-    --optimizer adam \
-    --adam-betas '(0.9,0.98)' \
-    --clip-norm 0.0 \
-    --lr-scheduler inverse_sqrt \
-    --warmup-init-lr 1e-07 \
-    --warmup-updates 1 \
-    --pretrained-embed-dim 768 \
-    --lr 0.025 \
-    --min-lr 1e-09 \
-    --dropout 0.0 \
-    --weight-decay 0.0 \
-    --criterion label_smoothed_cross_entropy \
-    --label-smoothing 0.01 \
-    --keep-last-epochs 1 \
-    --max-tokens 3584 \
-    --log-format json \
-    --fp16 \
-    --seed 42 \
-    --save-dir $MODEL_FOLDER 
+# # ORACLE EXTRACTION
+# # Given sentence and aligned AMR, provide action sequence that generates the
+# # AMR back
+# mkdir -p $ORACLE_FOLDER
+# amr-oracle \
+#     --in-amr DATA/wiki25.jkaln \
+#     --out-sentences $ORACLE_FOLDER/train.en \
+#     --out-actions $ORACLE_FOLDER/train.actions \
+#     --out-rule-stats $ORACLE_FOLDER/train.rules.json \
+#     --copy-lemma-action
+# # For this unit test train/dev/test is the same (memorize test)
+# cp $ORACLE_FOLDER/train.en $ORACLE_FOLDER/dev.en
+# cp $ORACLE_FOLDER/train.actions $ORACLE_FOLDER/dev.actions
+# cp $ORACLE_FOLDER/train.en $ORACLE_FOLDER/test.en
+# cp $ORACLE_FOLDER/train.actions $ORACLE_FOLDER/test.actions
+# 
+# # PREPROCESSING
+# # Extract sentence featrures and action sequence and store them in fairseq
+# # format
+# rm -Rf DATA.tests/features/wiki25/  # not as var for security
+# mkdir -p $FEATURES_FOLDER
+# fairseq-preprocess \
+#     --source-lang en \
+#     --target-lang actions \
+#     --trainpref $ORACLE_FOLDER/train \
+#     --validpref $ORACLE_FOLDER/dev \
+#     --testpref $ORACLE_FOLDER/test \
+#     --destdir $FEATURES_FOLDER \
+#     --workers 1 \
+#     --machine-type AMR \
+#     --machine-rules $ORACLE_FOLDER/train.rules.json 
+#  
+# # TRAINING
+# rm -Rf DATA.tests/models/wiki25/
+# fairseq-train \
+#     $FEATURES_FOLDER \
+#     --max-epoch $max_epoch \
+#     --arch stack_transformer_6x6_tops_nopos \
+#     --optimizer adam \
+#     --adam-betas '(0.9,0.98)' \
+#     --clip-norm 0.0 \
+#     --lr-scheduler inverse_sqrt \
+#     --warmup-init-lr 1e-07 \
+#     --warmup-updates 1 \
+#     --pretrained-embed-dim 768 \
+#     --lr 0.025 \
+#     --min-lr 1e-09 \
+#     --dropout 0.0 \
+#     --weight-decay 0.0 \
+#     --criterion label_smoothed_cross_entropy \
+#     --label-smoothing 0.01 \
+#     --keep-last-epochs 1 \
+#     --max-tokens 3584 \
+#     --log-format json \
+#     --fp16 \
+#     --seed 42 \
+#     --save-dir $MODEL_FOLDER 
 
 # DECODING
 rm -Rf DATA.tests/models/wiki25/beam1
 mkdir -p $RESULTS_FOLDER
+# --nbest 3 \
 fairseq-generate \
     $FEATURES_FOLDER  \
     --gen-subset valid \
