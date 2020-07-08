37ecca4140fb17deee57552c6bc5b77748defff8
gangiswag <g.revanthreddy111@gmail.com>
2020-07-08 17:12:38 -0500

    Update deployment to handle decoupled arguments

--- service/stack-transformer/amr_server.py
+++ service/stack-transformer/amr_server.py
@@ -11,48 +11,61 @@ from transition_amr_parser.stack_transformer_amr_parser import AMRParser
 from fairseq import options
 import argparse
 
-def add_server_arguments(parser):
+def argument_parser():
+    parser = argparse.ArgumentParser(description='AMR parser')
+    parser.add_argument(
+        "--in-model",
+        help="path to the AMR parsing model",
+        type=str
+    )
     parser.add_argument(
         "--roberta-cache-path",
         help="Path to the roberta large model",
         type=str
     )
     parser.add_argument(
-        "--roberta-batch-size",
-        help="Batch size to compute roberta embeddings",
-        type=int
-    )
-    parser.add_argument(
         "--port",
         help="GRPC port",
         type=str
     )
-    # for pretrained external embeddings
-    parser.add_argument("--pretrained-embed", default='roberta.base',
-                       help="Type of pretrained embedding")
-    # NOTE: Previous default "17 18 19 20 21 22 23 24"
-    parser.add_argument('--bert-layers', nargs='+', type=int,
-                       help='RoBERTa layers to extract (default last)')
+    parser.add_argument(
+        '--roberta-batch-size',
+        type=int,
+        default=10,
+        help='Batch size for roberta computation (watch for OOM)'
+    )
+    parser.add_argument(
+        '--batch-size',
+        type=int,
+        default=64,
+        help='Batch size for decoding (excluding roberta)'
+    )
+    args = parser.parse_args()
+
+    # Sanity checks
+    assert args.in_model
+    assert args.port
+
+    return args
 
 class Parser():
 
     def __init__(self, args):
-        self.parser = AMRParser(args)
+        self.parser = AMRParser.from_checkpoint(checkpoint=args.in_model, roberta_cache_path=args.roberta_cache_path)
+        self.batch_size = args.batch_size
+        self.roberta_batch_size = args.roberta_batch_size
 
     def process(self, request, context):
         sentences = request.sentences
         batch = []
         for sentence in sentences:
             batch.append(sentence.tokens)
-        amrs = self.parser.parse_sentences(batch)
+        amrs = self.parser.parse_sentences(batch, batch_size=self.batch_size, roberta_batch_size=self.roberta_batch_size)
         return amr_pb2.AMRBatchResponse(amr_parse=amrs)
 
 def serve():
     # Argument handling
-    parser = options.get_interactive_generation_parser()
-    options.add_optimization_args(parser)
-    add_server_arguments(parser)
-    args = options.parse_args_and_arch(parser)
+    args = argument_parser()
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
     amr_pb2_grpc.add_AMRBatchServerServicer_to_server(Parser(args), server)
