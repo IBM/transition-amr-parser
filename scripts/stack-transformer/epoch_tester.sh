02d14d4849d34283db9e6e6e8c9e6c2e09cf22d3
ramon-astudillo <ramon@astudillo.com>
2020-08-04 14:22:45 -0400

    Minor aesthetic changes

--- scripts/stack-transformer/epoch_tester.sh
+++ scripts/stack-transformer/epoch_tester.sh
@@ -67,7 +67,7 @@ for test_model in $(find $checkpoints_folder -iname 'checkpoint[0-9]*.pt' | sort
 	fi
         # will come to bite us in the future
         amr-fake-parse \
-	    --entity-rules $ENTITY_RULES \
+	        --entity-rules $ENTITY_RULES \
             --in-sentences $ORACLE_FOLDER/dev.en \
             --in-actions ${std_name}.actions \
             --out-amr ${std_name}.amr 
