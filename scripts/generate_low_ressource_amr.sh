546d1c23bba8a7119aa4e320d5845f3ff3db5b23
ramon-astudillo <ramon@astudillo.com>
2020-08-11 14:25:15 -0400

    Remove debugging comment

--- scripts/generate_low_ressource_amr.sh
+++ scripts/generate_low_ressource_amr.sh
@@ -10,8 +10,7 @@ set -o nounset
 # TRAIN DATA
 
 LDC2016_AMR_CORPUS=/dccstor/ykt-parse/SHARED/CORPORA/AMR/LDC2016T10_preprocessed_tahira/
-#OUT_FOLDER=/dccstor/ykt-parse/SHARED/CORPORA/AMR/low_ressource/amr2.0_from_amr1.0_v0/
-OUT_FOLDER=TMP
+OUT_FOLDER=/dccstor/ykt-parse/SHARED/CORPORA/AMR/low_ressource/amr2.0_from_amr1.0_v0/
  
 # Split AMR2.0 with o3 alignments into a set with AMR1.0 ids and one without
 mkdir -p amr2.0_from_amr1.0/ amr2.0_minus_amr1.0/
