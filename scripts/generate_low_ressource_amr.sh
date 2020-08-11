8f6a76edcc2bbb578c705b534f69cf78318bf26c
ramon-astudillo <ramon@astudillo.com>
2020-08-11 14:22:52 -0400

    Update to use --entity-rules

--- scripts/generate_low_ressource_amr.sh
+++ scripts/generate_low_ressource_amr.sh
@@ -1,6 +1,6 @@
 set -o pipefail
 set -o errexit
-#. set_environment.sh
+. set_environment.sh
 set -o nounset
 
 #
@@ -10,7 +10,8 @@ set -o nounset
 # TRAIN DATA
 
 LDC2016_AMR_CORPUS=/dccstor/ykt-parse/SHARED/CORPORA/AMR/LDC2016T10_preprocessed_tahira/
-OUT_FOLDER=/dccstor/ykt-parse/SHARED/CORPORA/AMR/low_ressource/amr2.0_from_amr1.0_v0/
+#OUT_FOLDER=/dccstor/ykt-parse/SHARED/CORPORA/AMR/low_ressource/amr2.0_from_amr1.0_v0/
+OUT_FOLDER=TMP
  
 # Split AMR2.0 with o3 alignments into a set with AMR1.0 ids and one without
 mkdir -p amr2.0_from_amr1.0/ amr2.0_minus_amr1.0/
@@ -20,10 +21,16 @@ python scripts/amr2.0_from_amr1.0.py \
     $OUT_FOLDER/amr2.0_from_amr1.0_train_jkaln.amr \
     $OUT_FOLDER/amr2.0_minus_amr1.0_train_jkaln.amr
 
+# Extract entity rules for the entire set
+python scripts/extract_rules.py \
+    $LDC2016_AMR_CORPUS/jkaln_2016_scr.txt \
+    $OUT_FOLDER/jkaln_2016_scr.entity_rules.json
+
 # Create oracles (`o3`) without labeled SHIFT for the 25K sentences outside of
 # AMR1.0
 amr-oracle \
     --in-amr $OUT_FOLDER/amr2.0_minus_amr1.0_train_jkaln.amr \
+    --entity-rules $OUT_FOLDER/jkaln_2016_scr.entity_rules.json \
     --out-sentences $OUT_FOLDER/train.en \
     --out-actions $OUT_FOLDER/train.actions \
     --out-rule-stats $OUT_FOLDER/train.rules.json \
@@ -33,6 +40,7 @@ amr-oracle \
 amr-fake-parse \
     --in-sentences $OUT_FOLDER/train.en \
     --in-actions $OUT_FOLDER/train.actions \
+    --entity-rules $OUT_FOLDER/jkaln_2016_scr.entity_rules.json \
     --out-bio-tags $OUT_FOLDER/amr2.0_minus_amr1.0_train_jkaln.tags
 
 # Remove oracle
@@ -50,6 +58,7 @@ for sset in dev test;do
     
     amr-oracle \
         --in-amr $LDC2016_AMR_CORPUS/${sset}.txt.removedWiki.noempty.JAMRaligned \
+        --entity-rules $OUT_FOLDER/jkaln_2016_scr.entity_rules.json \
         --out-sentences $OUT_FOLDER/${sset}.en \
         --out-actions $OUT_FOLDER/${sset}.actions \
         --copy-lemma-action
@@ -59,6 +68,7 @@ for sset in dev test;do
     amr-fake-parse \
         --in-sentences $OUT_FOLDER/${sset}.en \
         --in-actions $OUT_FOLDER/${sset}.actions \
+        --entity-rules $OUT_FOLDER/jkaln_2016_scr.entity_rules.json \
         --out-bio-tags $OUT_FOLDER/${sset}_jkaln.tags
 
     python scripts/tasks_from_amr_tags.py \
