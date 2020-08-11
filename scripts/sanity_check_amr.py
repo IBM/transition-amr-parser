8bff5a3b5cc89d0c9f299c8e58358e693b097cb8
ramon-astudillo <ramon@astudillo.com>
2020-08-11 14:26:22 -0400

    Print alerts

--- scripts/sanity_check_amr.py
+++ scripts/sanity_check_amr.py
@@ -29,10 +29,12 @@ if __name__ == '__main__':
 
     amr_alerts = defaultdict(list)
     sid = 0
+    num_preds = 0
     for amr in tqdm(corpus.amrs):
         predicate_ids = [
             k for k, v in amr.nodes.items() if pred_regex.match(v)
         ]
+        num_preds += len(predicate_ids)
         for pred_id in predicate_ids:
             pred = get_propbank_name(amr.nodes[pred_id])
             if pred not in propbank:
@@ -42,21 +44,36 @@ if __name__ == '__main__':
             else:    
                 probank_roles = propbank[pred]['roles']
                 # TODO: Identify obligatory args
-#                 for k, v in probank_roles.items():
-#                     if 'must' in v['descr']:
-#                         import ipdb; ipdb.set_trace(context=30)
-#                         print()
+                required_roles = set()
+                required_location = set()
+                for k, v in probank_roles.items():
+                    if '(must be specified)' in v['descr']:
+                        required_roles |= set([k])
+                    elif 'must' in v['descr']:
+                        # FIXME: not used right now
+                        required_location = set([k])
+
+                # Get roles
                 roles = [
                     trip[1][1:].replace('-of', '') 
                     for trip in amr.edges 
                     if trip[0] == pred_id and trip[1].startswith(':ARG')
                 ]
+                # Check no required missing
+                missing_roles = required_roles - set(roles)
+                if missing_roles:
+                    amr_alerts['missing required role'].append(
+                        (sid, pred_id, pred, " ".join(list(missing_roles)))
+                    )
+                # Check no forbiden used
                 forbidden_roles = set(roles) - set(probank_roles.keys())
                 if forbidden_roles:
                     amr_alerts['role not in propbank'].append(
                         (sid, pred_id, pred, " ".join(list(forbidden_roles)))
                     )
-            sid += 1
+        sid += 1
 
-    import ipdb; ipdb.set_trace(context=30)
-    print()
+    print(f'{sid+1} sentences {num_preds} predicates')
+    for name, alerts in amr_alerts.items():
+        if alerts:
+            print(f'{len(alerts)} {name}')
