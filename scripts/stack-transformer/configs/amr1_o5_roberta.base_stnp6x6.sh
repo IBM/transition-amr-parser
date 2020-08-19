b79c05667802625f8b73b6debe28518a9c765a36
ramon-astudillo <ramon@astudillo.com>
2020-08-19 19:13:03 -0400

    Rename/remove basic experiments configs

--- scripts/stack-transformer/configs/amr1_o5_roberta.base_stnp6x6.sh
+++ scripts/stack-transformer/configs/amr1_o5_roberta.base_stnp6x6.sh
@@ -6,68 +6,53 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-# NOTE: Assumes you ran training with
-#
-# scripts/stack-transformer/configs/QALD_prepro.sh
-#
-# to get (see use below)
-#
-# DATA/AMR/features/ldcqbqaldlarge_o5+Word100_RoBERTa-large-top24/dict.en.txt
-# DATA/AMR/features/ldcqbqaldlarge_o5+Word100_RoBERTa-large-top24/dict.actions.txt
-# DATA/AMR/models/ldcqbqaldlarge_o5+Word100_RoBERTa-large-top24_stnp6x6-seed44/checkpoint_best_SMATCH.pt
-#
-
 TASK_TAG=AMR
-
-# Global paths
-# Global paths
-AMR_CORPORA=/dccstor/ykt-parse/SHARED/CORPORA/AMR/
 
 # All data stored here
 data_root=DATA/$TASK_TAG/
 
+LDC2014_AMR_CORPUS=/dccstor/ykt-parse/SHARED/CORPORA/AMR/AMR_1.0/
+
 # AMR ORACLE
 # See transition_amr_parser/data_oracle.py:argument_parser
-AMR_TRAIN_FILE=$AMR_CORPORA/QB20200305/qb.pseudo.aln
-AMR_DEV_FILE=$AMR_CORPORA/QB20200305/qald_dev2_pass3.jaln
-AMR_TEST_FILE=$AMR_CORPORA/QB20200305/blindtest.jkaln
+# NOTE: LDC2016_AMR_CORPUS should be defined in set_envinroment.sh
+AMR_TRAIN_FILE=$LDC2014_AMR_CORPUS/AMR_1.0_train_jkaln_pseudo.txt
+AMR_DEV_FILE=$LDC2014_AMR_CORPUS/AMR_1.0_dev_jaln.txt
+AMR_TEST_FILE=$LDC2014_AMR_CORPUS/AMR_1.0_test_jaln.txt
 # WIKI files
 # NOTE: If left empty no wiki will be added
 WIKI_DEV=""
-AMR_DEV_FILE_WIKI="" 
+AMR_DEV_FILE_WIKI=""
 WIKI_TEST=""
 AMR_TEST_FILE_WIKI=""
+# Entity rules
+# Leave empty to create entity rules from the corpus
+ENTITY_RULES=""
 
 # Labeled shift: each time we shift, we also predict the word being shited
 # but restrict this to top MAX_WORDS. Controlled by
 # --multitask-max-words --out-multitask-words --in-multitask-words
 # To have an action calling external lemmatizer (SpaCy)
 # --copy-lemma-action
-MAX_WORDS=100
-ORACLE_TAG=qbqaldlargefinetune_o5+Word${MAX_WORDS}
+ORACLE_TAG=amr1_o5
 ORACLE_FOLDER=$data_root/oracles/${ORACLE_TAG}/
 ORACLE_TRAIN_ARGS="
-    --multitask-max-words $MAX_WORDS 
-    --out-multitask-words $ORACLE_FOLDER/train.multitask_words 
     --copy-lemma-action
 "
 ORACLE_DEV_ARGS="
-    --in-multitask-words $ORACLE_FOLDER/train.multitask_words \
     --copy-lemma-action
 "
 
+# GPU
+# k80, v100 (3 times faster)
 
 # PREPROCESSING
 # See fairseq/fairseq/options.py:add_preprocess_args
-PREPRO_TAG="RoBERTa-large-top24"
+PREPRO_TAG="RoBERTa-base"
 # CCC configuration in scripts/stack-transformer/jbsub_experiment.sh
 PREPRO_GPU_TYPE=v100
-PREPRO_QUEUE=x86_24h
+PREPRO_QUEUE=x86_6h
 features_folder=$data_root/features/${ORACLE_TAG}_${PREPRO_TAG}/
-# TODO: Get this paths refred to the SHARED folder
-# ${AMR_MODELS}/features/qaldlarge_extracted/
-srcdict="$data_root/features/ldcqbqaldlarge_o5+Word100_RoBERTa-large-top24/dict.en.txt"
-tgtdict="$data_root/features/ldcqbqaldlarge_o5+Word100_RoBERTa-large-top24/dict.actions.txt"
 FAIRSEQ_PREPROCESS_ARGS="
     --source-lang en
     --target-lang actions
@@ -75,14 +60,10 @@ FAIRSEQ_PREPROCESS_ARGS="
     --validpref $ORACLE_FOLDER/dev
     --testpref $ORACLE_FOLDER/test
     --destdir $features_folder
-    --workers 1 
-    --pretrained-embed roberta.large
-    --bert-layers 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
-    --srcdict $srcdict
-    --tgtdict $tgtdict
+    --workers 1
+    --pretrained-embed roberta.base
     --machine-type AMR 
     --machine-rules $ORACLE_FOLDER/train.rules.json 
-    --fp16
 "
 
 # TRAINING
@@ -98,13 +79,10 @@ TRAIN_QUEUE=ppc_24h
 # --lazy-load for very large corpora (data does not fit into RAM)
 # --bert-backprop do backprop though BERT
 # NOTE: --save-dir is specified inside dcc/train.sh to account for the seed
-MAX_EPOCH=190
+MAX_EPOCH=100
 CHECKPOINTS_DIR_ROOT="$data_root/models/${ORACLE_TAG}_${PREPRO_TAG}_${TRAIN_TAG}"
-# NOTE: We start from a pretrained model
-pretrained="$data_root/models/ldcqbqaldlarge_o5+Word100_RoBERTa-large-top24_stnp6x6-seed44/checkpoint_best_SMATCH.pt"
 FAIRSEQ_TRAIN_ARGS="
     $features_folder
-    --restore-file $pretrained
     --max-epoch $MAX_EPOCH
     --arch $base_model
     --optimizer adam
@@ -113,17 +91,17 @@ FAIRSEQ_TRAIN_ARGS="
     --lr-scheduler inverse_sqrt
     --warmup-init-lr 1e-07
     --warmup-updates 4000
-    --pretrained-embed-dim 1024
+    --pretrained-embed-dim 768
     --lr 0.0005
     --min-lr 1e-09
     --dropout 0.3
     --weight-decay 0.0
     --criterion label_smoothed_cross_entropy
     --label-smoothing 0.01
-    --keep-last-epochs 100
+    --keep-last-epochs 40
     --max-tokens 3584
     --log-format json
-    --reset-optimizer
+    --fp16
 "
 
 # TESTING
@@ -135,7 +113,7 @@ TEST_TAG="beam${beam_size}"
 CHECKPOINT=checkpoint_best.pt
 # CCC configuration in scripts/stack-transformer/jbsub_experiment.sh
 TEST_GPU_TYPE=v100
-TEST_QUEUE=x86_6h
+TEST_QUEUE=x86_12h
 FAIRSEQ_GENERATE_ARGS="
     $features_folder 
     --gen-subset valid
