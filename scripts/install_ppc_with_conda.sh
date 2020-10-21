ac39efc68c441b827e96d92288fb5ff7329a2132
ramon-astudillo <ramon@astudillo.com>
2020-10-21 10:35:26 -0400

    WIP public fairseq installer

--- scripts/install_ppc_with_conda.sh
+++ scripts/install_ppc_with_conda.sh
@@ -5,45 +5,27 @@ set -o pipefail
 . set_environment.sh
 set -o nounset 
 
-# use this environment for debugging (comment line above)
-# eval "$(/path/to/ppc64/miniconda3/bin/conda shell.bash hook)"
-# rm -Rf ./tmp_debug
-# conda create -y -p ./tmp_debug
-# conda activate ./tmp_debug
-
 # install python version to be used
 conda install python=3.6.9 -y -c powerai
 
 # pre-install modules with conda 
-conda env update -f scripts/stack-transformer/ccc_ppc_fairseq.yml
+conda env update -f scripts/stack-transformer/environment.yml
+# Note spacy only available with conda. Version will not match x86 one
+conda install spacy -y -c defaults -c https://public.dhe.ibm.com/ibmdl/export/pub/software/server/ibm-ai/conda -c powerai 
+pip install smatch==1.0.4 ipdb
 
-# fairseq
-[ ! -d fairseq ] && git clone git@github.ibm.com:ramon-astudillo/fairseq.git
-cd fairseq
-git checkout v0.3.0/decouple-fairseq
+# download fairseq
+# public branch and patch
+bash scripts/download_and_patch_fairseq.sh
+# private branch (for development)
+# git clone git@github.ibm.com:ramon-astudillo/fairseq.git fairseq-stack-transformer-v0.3.2
+# cd fairseq-stack-transformer-v0.3.2
+# git checkout v0.3.0/decouple-fairseq
+# cd ..
+
+# install repos
+pip install --no-deps --editable fairseq-stack-transformer-v0.3.2
 pip install --editable .
-cd ..
 
-# transition_amr_parser
-pip install ipdb
-pip install --no-deps --editable . 
-
-# install pytorch scatter
-rm -Rf pytorch_scatter.ppc
-git clone https://github.com/rusty1s/pytorch_scatter.git pytorch_scatter.ppc
-cd pytorch_scatter.ppc
-git checkout 1.3.2
-# Ensure modern GCC
-export GCC_DIR=/opt/share/gcc-5.4.0/ppc64le/
-export PATH=/opt/share/cuda-9.0/ppc64le/bin:$GCC_DIR/bin:$PATH
-export LD_LIBRARY_PATH=$GCC_DIR/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$GCC_DIR/lib64:$LD_LIBRARY_PATH
-python setup.py develop
-cd ..
-
-# smatch v1.0.4
-[ ! -d smatch ] && git clone https://github.com/snowblink14/smatch.git smatch
-cd smatch
-git checkout v1.0.4
-cd ..
-pip install smatch/
+# sanity check
+python tests/correctly_installed.py
