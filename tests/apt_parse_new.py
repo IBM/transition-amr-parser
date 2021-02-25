bc1e5c827f50a88e9aa09c9ca53d88daf1bafd2b
ramon-astudillo <ramon@astudillo.com>
2021-02-25 18:10:02 -0500

    Rename APT parser as parse

--- tests/apt_parse_new.py
+++ tests/apt_parse_new.py
@@ -1,4 +1,4 @@
-from transition_amr_parser.apt_amr_parser import AMRParser
+from transition_amr_parser.parse import AMRParser
 
 
 in_checkpoint = 'DATA/AMR2.0/models/exp_cofill_o8.3_act-states_RoBERTa-large-top24/_act-pos-grh_vmask1_shiftpos1_ptr-lay6-h1_grh-lay123-h2-allprev_1in1out_cam-layall-h2-abuf/ep120-seed42/checkpoint_wiki.smatch_top5-avg.pt'
