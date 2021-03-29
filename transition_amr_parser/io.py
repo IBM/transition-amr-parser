7f2a5cda5ea1870addda97d11c753fc023c9db2b
ramon-astudillo <ramon@astudillo.com>
2021-03-29 17:04:33 -0400

    Add support for individual seed progress report

--- transition_amr_parser/io.py
+++ transition_amr_parser/io.py
@@ -1,10 +1,125 @@
 import re
 import json
-import subprocess
 from collections import Counter
+import subprocess
 from transition_amr_parser.amr import JAMR_CorpusReader
 import ast
 import xml.etree.ElementTree as ET
+
+import shutil
+import numpy as np
+
+
+def clbar(
+    xy=None,  # list of (x, y) tuples or Counter
+    x=None,
+    y=None,
+    ylim=(None, None),
+    ncol=None,    # Max number of lines for display (defauly window size)
+    # show only top and bottom values
+    topx=None,
+    botx=None,
+    topy=None,
+    boty=None,
+    # normalize to sum to 1
+    norm=False,
+    xfilter=None,  # f(x) returns bool to not skip this example in display
+    yform=None     # Function receiveing single y value returns string
+):
+    """Print data structure in command line"""
+    # Sanity checks
+    if x is None and y is None:
+        if isinstance(xy, np.ndarray):
+            labels = [f'{i}' for i in range(xy.shape[0])]
+            xy = list(zip(labels, list(xy)))
+        elif isinstance(xy, Counter):
+            xy = [(str(x), y) for x, y in xy.items()]
+        else:
+            assert isinstance(xy, list), "Expected list of tuples"
+            assert isinstance(xy[0], tuple), "Expected list of tuples"
+    else:
+        assert x is not None and y is not None
+        assert isinstance(x, list)
+        assert isinstance(y, list) or isinstance(y, np.ndarray)
+        assert len(x) == len(list(y))
+        xy = list(zip(x, y))
+
+    # normalize
+    if norm:
+        z = sum([x[1] for x in xy])
+        xy = [(k, v / z) for k, v in xy]
+    # show only top x
+    if topx is not None:
+        xy = sorted(xy, key=lambda x: float(x[0]))[-topx:]
+    if botx is not None:
+        xy = sorted(xy, key=lambda x: float(x[0]))[:botx]
+    if boty is not None:
+        xy = sorted(xy, key=lambda x: x[1])[:boty]
+    if topy is not None:
+        xy = sorted(xy, key=lambda x: x[1])[-topy:]
+    # print list of tuples
+    # determine variables to fit data to command line
+    x_data, y_data = zip(*xy)
+    width = max([len(x) if x is not None else len('None') for x in x_data])
+    number_width = max([len(f'{y}') for y in y_data])
+    # max and min values
+    if ylim[1] is not None:
+        max_y_data = ylim[1]
+    else:
+        max_y_data = max(y_data)
+    if ylim[0] is not None:
+        min_y_data = ylim[0]
+    else:
+        min_y_data = min(y_data)
+    # determine scaling factor from screen size
+    data_range = max_y_data - min_y_data
+    if ncol is None:
+        ncol, _ = shutil.get_terminal_size((80, 20))
+    max_size = ncol - width - number_width - 3
+    scale = max_size / data_range
+    # plot
+    print()
+    blank = ' '
+    if yform:
+        min_y_data_str = yform(min_y_data)
+        print(f'{blank:<{width}}{min_y_data_str}')
+    else:
+        print(f'{blank:<{width}}{min_y_data}')
+    for (x, y) in xy:
+
+        # Filter example by x
+        if xfilter and not xfilter(x):
+            continue
+
+        if y > max_y_data:
+            # cropped bars
+            num_col = int((ylim[1] - min_y_data) * scale)
+            if num_col == 0:
+                bar = ''
+            else:
+                half_width = (num_col // 2)
+                if num_col % 2:
+                    bar = '\u25A0' * (half_width - 1)
+                    bar += '//'
+                    bar += '\u25A0' * (half_width - 1)
+                else:
+                    bar = '\u25A0' * half_width
+                    bar += '//'
+                    bar += '\u25A0' * (half_width - 1)
+        else:
+            bar = '\u25A0' * int((y - min_y_data) * scale)
+        if x is None:
+            x = 'None'
+        if yform:
+            y = yform(y)
+            print(f'{x:<{width}} {bar} {y}')
+        else:
+            print(f'{x:<{width}} {bar} {y}')
+    print()
+
+
+def yellow_font(string):
+    return "\033[93m%s\033[0m" % string
 
 
 def read_frame(xml_file):
@@ -58,7 +173,6 @@ def read_frame(xml_file):
                     'tokens': tokens,
                     'args': args
                 })
-
 
     return propbank
 
@@ -143,12 +257,13 @@ def write_action_scores(file_path, action_scores):
             score = items[1:4]
             smatch = items[4]
             length = items[5]
-            reason = items [6]
+            reason = items[6]
             actions = items[7]
             if actions is not None:
                 actions = '\t'.join(actions)
             fid.write(
-                f'{sid} {score[0]} {score[1]} {score[2]} {smatch} {length} {reason} {actions}\n'
+                f'{sid} {score[0]} {score[1]} {score[2]} {smatch} {length}'
+                f' {reason} {actions}\n'
             )
 
 
@@ -187,7 +302,8 @@ def read_rule_stats(rule_stats_json):
     with open(rule_stats_json) as fid:
         rule_stats = json.loads(fid.read())
     # convert to counters
-    rule_stats['possible_predicates'] = Counter(rule_stats['possible_predicates'])
+    rule_stats['possible_predicates'] = \
+        Counter(rule_stats['possible_predicates'])
     rule_stats['action_vocabulary'] = Counter(rule_stats['action_vocabulary'])
     return rule_stats
 
