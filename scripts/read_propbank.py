fe6fb8ad74ac588715288d69f85a528f40b2a078
ramon-astudillo <ramon@astudillo.com>
2021-07-05 17:00:42 -0400

    Recover files from v0.4.2

---
+++ scripts/read_propbank.py
@@ -0,0 +1,28 @@
+import sys
+import glob
+import json
+from tqdm import tqdm
+from transition_amr_parser.io import read_frame
+
+if __name__ == '__main__':
+
+    # Argument handling
+    in_propank_folder, out_json = sys.argv[1:]
+
+    # Read propbank into dict
+    propbank = {}
+    num_files = 0
+    for xml_file in tqdm(glob.glob(f'{in_propank_folder}/*.xml')):
+        propbank.update(read_frame(xml_file))
+        num_files += 1
+    if not num_files:
+        print('No XML files found!')
+        exit(1)
+
+    num_preds = len(propbank)
+    num_examples = sum([len(x['examples']) for x in propbank.values()])
+    print(f'{num_files} files {num_preds} predicates {num_examples} examples read')
+
+    # Write it into json
+    with open(out_json, 'w') as fid:
+        fid.write(json.dumps(propbank))
