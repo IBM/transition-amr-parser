d22805811e49b7e8d3f8b2eee93c7484d8c8e842
ramon-astudillo <ramon@astudillo.com>
2021-07-27 17:32:14 -0400

    Reinstate aligining dev and test

--- run/aa_amr_actions.sh
+++ run/aa_amr_actions.sh
@@ -81,6 +81,7 @@ else
 
     # Dev
     echo "align dev"
+    # Get alignment probabilities
     python align_cfg/main.py --cuda \
         --no-jamr \
         --cache-dir $ALIGNED_FOLDER \
@@ -97,9 +98,35 @@ else
     python preprocess/remove_wiki.py \
 		$AMR_DEV_FILE_WIKI \
 		$ALIGNED_FOLDER/dev.txt
+
+    # Dev
+    echo "align dev"
+    python preprocess/remove_wiki.py $AMR_DEV_FILE_WIKI ${AMR_DEV_FILE_WIKI}.no_wiki
+    python align_cfg/main.py --cuda \
+        --no-jamr \
+        --cache-dir $ALIGNED_FOLDER \
+        --load $ALIGN_MODEL \
+        --load-flags $ALIGN_MODEL_FLAGS \
+        --vocab-text $ALIGN_VOCAB_TEXT \
+        --vocab-amr $ALIGN_VOCAB_AMR \
+        --write-single \
+        --single-input ${AMR_TRAIN_FILE_WIKI}.no_wiki \
+        --single-output $ALIGNED_FOLDER/dev.txt
+
     
     # Test
     echo "align test"
+    python align_cfg/main.py --cuda \
+        --no-jamr \
+        --cache-dir $ALIGNED_FOLDER \
+        --load $ALIGN_MODEL \
+        --load-flags $ALIGN_MODEL_FLAGS \
+        --vocab-text $ALIGN_VOCAB_TEXT \
+        --vocab-amr $ALIGN_VOCAB_AMR \
+        --write-single \
+        --single-input ${AMR_TRAIN_FILE_WIKI}.no_wiki \
+        --single-output $ALIGNED_FOLDER/test.txt
+
     python align_cfg/main.py --cuda \
         --no-jamr \
         --cache-dir $ALIGNED_FOLDER \
