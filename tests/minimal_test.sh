c6403f8ff032261f7b06fb597de935ed5fcc5727
ramon-astudillo <ramon@astudillo.com>
2021-07-29 19:00:51 -0400

    Fix OOV during on-the-fly oracle
    Adds training set node names and reverse arcs LA<->RA during vocabulary
    construction

--- tests/minimal_test.sh
+++ tests/minimal_test.sh
@@ -3,12 +3,15 @@ set -o pipefail
 set -o nounset 
 
 # Delete previous runs is exist
-#rm -Rf DATA/wiki25/*
-
+rm -Rf DATA/wiki25/*
 # simulate completed corpora extraction and alignment
-#bash tests/create_wiki25_mockup.sh
+bash tests/create_wiki25_mockup.sh
 
-# Run local test
+# replace code above with less restrictive deletion
+# rm -R -f DATA/wiki25/embeddings
+# rm -R -f DATA/wiki25/features
+# rm -R -f DATA/wiki25/oracles
+# rm -R -f DATA/wiki25/models
 
 #bash run/run_experiment.sh configs/wiki25.sh  
 bash run/run_experiment.sh configs/wiki25-neur-al-sampling.sh
