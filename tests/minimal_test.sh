6a94cdc66ea7ef6236909360dd58ccafc2c1dde3
ramon-astudillo <ramon@astudillo.com>
2021-08-09 18:10:58 -0400

    Post rebase fixes

--- tests/minimal_test.sh
+++ tests/minimal_test.sh
@@ -13,8 +13,8 @@ bash tests/create_wiki25_mockup.sh
 # rm -R -f DATA/wiki25/oracles
 # rm -R -f DATA/wiki25/models
 
-config=configs/wiki25-neur-al-sampling.sh
-# config=configs/wiki25.sh  
+# config=configs/wiki25-neur-al-sampling.sh
+config=configs/wiki25.sh  
 bash run/run_experiment.sh $config
 
 # check if final result is there
