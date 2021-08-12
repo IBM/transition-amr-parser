35baef4bc3113aad2010324e3d9b091ba8e32396
ramon-astudillo <ramon@astudillo.com>
2021-08-12 09:57:52 -0400

    wiki25 mockup removes JAMR notation from corpora

--- tests/create_wiki25_mockup.sh
+++ tests/create_wiki25_mockup.sh
@@ -6,9 +6,16 @@ set -o nounset
 rm -Rf DATA/wiki25/
 # Create original data
 mkdir -p DATA/wiki25/corpora/
-ln -s ../../wiki25.jkaln DATA/wiki25/corpora/train.txt 
-ln -s ../../wiki25.jkaln DATA/wiki25/corpora/dev.txt
-ln -s ../../wiki25.jkaln DATA/wiki25/corpora/test.txt 
+cp DATA/wiki25.jkaln DATA/wiki25/corpora/train.txt 
+# remove JAMR meta-data, which we do not have in reality
+#sed '/^# ::tok.*/d' -i DATA/wiki25/corpora/train.txt
+sed '/^# ::node.*/d' -i DATA/wiki25/corpora/train.txt
+sed '/^# ::edge.*/d' -i DATA/wiki25/corpora/train.txt
+sed '/^# ::root.*/d' -i DATA/wiki25/corpora/train.txt
+sed '/^# ::alignments.*/d' -i DATA/wiki25/corpora/train.txt
+ln -s DATA/wiki25/corpora/train.txt DATA/wiki25/corpora/dev.txt
+ln -s DATA/wiki25/corpora/train.txt DATA/wiki25/corpora/test.txt 
+
 touch DATA/wiki25/corpora/.done
 
 # Simulate aligned data from wiki25
