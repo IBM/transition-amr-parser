841f5d5afaf867626d7f22ded7eebbe77d5f07d4
ramon-astudillo <ramon@astudillo.com>
2022-04-12 16:22:08 -0400

    Add cl and LSF aligining scripts

---
+++ run/align.sh
@@ -0,0 +1,13 @@
+set -o pipefail
+set -o errexit
+. set_environment.sh
+HELP="$0 <checkpoint> <in_amr> <out_amr>"
+[ -z $1 ] && echo "$HELP" && exit 1
+[ -z $2 ] && echo "$HELP" && exit 1
+[ -z $3 ] && echo "$HELP" && exit 1
+checkpoint=$1
+in_amr=$2
+out_amr=$3
+set -o nounset
+
+amr-parse --in-checkpoint $checkpoint --in-amr $in_amr --out-amr $out_amr
