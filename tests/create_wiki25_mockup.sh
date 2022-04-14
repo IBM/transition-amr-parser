d6bf52b9d62f11179ec25f8f18acd1784f11658b
ramon-astudillo <ramon@astudillo.com>
2022-04-14 16:49:50 -0400

    Add OSX support for tests/create_wiki25_mockup.sh

--- tests/create_wiki25_mockup.sh
+++ tests/create_wiki25_mockup.sh
@@ -7,19 +7,24 @@ set -o nounset
 mkdir -p DATA/wiki25/corpora/
 cp DATA/wiki25.jkaln DATA/wiki25/corpora/train.txt 
 # remove JAMR meta-data, which we do not have in reality
-sed '/^# ::tok.*/d' -i DATA/wiki25/corpora/train.txt
-sed '/^# ::node.*/d' -i DATA/wiki25/corpora/train.txt
-sed '/^# ::edge.*/d' -i DATA/wiki25/corpora/train.txt
-sed '/^# ::root.*/d' -i DATA/wiki25/corpora/train.txt
-sed '/^# ::alignments.*/d' -i DATA/wiki25/corpora/train.txt
-ln -s ./train.txt DATA/wiki25/corpora/dev.txt
-ln -s ./train.txt DATA/wiki25/corpora/test.txt 
+sed -i.bak '/^# ::tok.*/d' DATA/wiki25/corpora/train.txt
+sed -i.bak '/^# ::node.*/d' DATA/wiki25/corpora/train.txt
+sed -i.bak '/^# ::edge.*/d' DATA/wiki25/corpora/train.txt
+sed -i.bak '/^# ::root.*/d' DATA/wiki25/corpora/train.txt
+sed -i.bak '/^# ::alignments.*/d' DATA/wiki25/corpora/train.txt
+[ ! -f DATA/wiki25/corpora/dev.txt ] \
+    && ln -s ./train.txt DATA/wiki25/corpora/dev.txt
+[ ! -f DATA/wiki25/corpora/test.txt ] \
+    && ln -s ./train.txt DATA/wiki25/corpora/test.txt 
 
 touch DATA/wiki25/corpora/.done
 
 # Simulate aligned data from wiki25
 mkdir -p DATA/wiki25/aligned/cofill/
-ln -s ../../../wiki25.jkaln DATA/wiki25/aligned/cofill/train.txt 
-ln -s ../../../wiki25.jkaln DATA/wiki25/aligned/cofill/dev.txt
-ln -s ../../../wiki25.jkaln DATA/wiki25/aligned/cofill/test.txt 
+[ ! -f DATA/wiki25/aligned/cofill/train.txt ] \
+    && ln -s ../../../wiki25.jkaln DATA/wiki25/aligned/cofill/train.txt 
+[ ! -f DATA/wiki25/aligned/cofill/train.txt ] \
+    && ln -s ../../../wiki25.jkaln DATA/wiki25/aligned/cofill/train.txt
+[ ! -f DATA/wiki25/aligned/cofill/test.txt ] \
+    && ln -s ../../../wiki25.jkaln DATA/wiki25/aligned/cofill/test.txt 
 touch DATA/wiki25/aligned/cofill/.done
