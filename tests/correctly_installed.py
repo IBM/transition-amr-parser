8589283590e5a3bf586aea729438cbe61dd2dc9f
ramon-astudillo <ramon@astudillo.com>
2022-05-10 20:57:46 -0400

    Remove check_cuda_torch_binary_vs_bare_metal (used deprecated torch API)

--- tests/correctly_installed.py
+++ tests/correctly_installed.py
@@ -3,36 +3,6 @@ import subprocess
 from ipdb import set_trace
 
 
-def check_cuda_torch_binary_vs_bare_metal():
-
-    # command line CUDA
-    cuda_dir = torch.utils.cpp_extension.CUDA_HOME
-    cuda_call = [cuda_dir + "/bin/nvcc", "-V"]
-    raw_output = subprocess.check_output(cuda_call, universal_newlines=True)
-    output = raw_output.split()
-    release_idx = output.index("release") + 1
-    release = output[release_idx].split(".")
-    bare_metal_major = release[0]
-    bare_metal_minor = release[1][0]
-
-    # torch compilation CUDA
-    torch_binary_major = torch.version.cuda.split(".")[0]
-    torch_binary_minor = torch.version.cuda.split(".")[1]
-
-    if (
-        (bare_metal_major != torch_binary_major)
-        or (bare_metal_minor != torch_binary_minor)
-    ):
-        print(
-            "Pytorch binaries were compiled with Cuda {} but binary {} is {}"
-            .format(
-                torch.version.cuda,
-                cuda_dir + "/bin/nvcc",
-                output[release_idx]
-            )
-        )
-
-
 def main():
 
     # Pytorch and CUDA
@@ -49,7 +19,6 @@ def main():
             print("Apex installed")
         except ImportError:
             print("Apex not installed")
-        check_cuda_torch_binary_vs_bare_metal()
         if torch.cuda.get_device_capability(0)[0] < 7:
             print("GPU wont support --fp")
 
