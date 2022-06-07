6a720abf87476c1bf356e0c33a4c3f1879b2b1d5
ramon-astudillo <ramon@astudillo.com>
2022-06-07 11:10:35 -0400

    Add check for correct exit in tests/all.sh

--- tests/all.sh
+++ tests/all.sh
@@ -1,4 +1,15 @@
 set -o errexit
+# This will ensure that early exit shows tests having failed
+function check_tests_passed {
+        if [ "$TESTS_PASSED" == "Y" ];then
+                printf "[\033[92m OK \033[0m] $0\n"
+        else
+                printf "[\033[91m FAILED \033[0m] $0\n"
+        fi
+}
+trap check_tests_passed EXIT
+TESTS_PASSED="N"
+
 # all conventional tests
 bash tests/correctly_installed.sh
 # small test with 25 sentences
@@ -7,6 +18,5 @@ bash tests/minimal_test.sh
 bash tests/standalone.sh
 # oracle for wiki25 imperfect due to alignments
 bash tests/oracles/amr_o10.sh DATA/wiki25/aligned/cofill_isi/train.txt
-
-# we need to reach here
-printf "[\033[92m OK \033[0m] $0\n"
+# if we reach here, we are good
+TESTS_PASSED="Y"
