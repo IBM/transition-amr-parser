77af2aed4e0b40d71b5ee78680f63c2c2c62d4e6
sadhana01 <sadhana_kumaravel@yahoo.co.in>
2022-06-08 14:53:02 -0400

    added fof path , changed machine

--- tests/oracles/amr_o10_doc.sh
+++ tests/oracles/amr_o10_doc.sh
@@ -9,7 +9,7 @@ oracle_folder=DATA/AMR3.0/oracles/o10_pinitos_doc_test/
 mkdir -p $oracle_folder 
  
 # get actions from oracle
-python transition_amr_parser/amr_machine_doc_fairseq.py \
+python transition_amr_parser/amr_machine_docamr.py \
     --in-aligned-amr $gold_amr \
     --out-machine-config $oracle_folder/machine_config.json \
     --out-actions $oracle_folder/train.actions \
@@ -17,11 +17,12 @@ python transition_amr_parser/amr_machine_doc_fairseq.py \
     --use-copy 1 \
     --absolute-stack-positions \
     --coref-fof DATA/AMR3.0/coref/train_coref.fof \
-    --norm no-merge
+    --norm no-merge \
+    --fof-path <path to AMR3.0> /
     # --reduce-nodes all
 
 # play actions on state machine
-python transition_amr_parser/amr_machine.py \
+python transition_amr_parser/amr_machine_docamr.py \
     --in-machine-config $oracle_folder/machine_config.json \
     --in-tokens $oracle_folder/train.tokens \
     --in-actions $oracle_folder/train.actions \
