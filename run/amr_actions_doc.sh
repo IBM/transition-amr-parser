15086b991b4809f5cb173d5481ef4d06469033ae
sadhana01 <sadhana_kumaravel@yahoo.co.in>
2022-07-08 10:35:35 -0400

    added modes, calling of docamr preprocess files

--- run/amr_actions_doc.sh
+++ run/amr_actions_doc.sh
@@ -31,9 +31,9 @@ else
     mkdir -p $ORACLE_FOLDER
 
     # copy the original AMR data: no wikification
-    cp $ALIGNED_FOLDER/train.txt $ORACLE_FOLDER/ref_train.amr
-    cp $ALIGNED_FOLDER/dev.txt $ORACLE_FOLDER/ref_dev.amr
-    cp $ALIGNED_FOLDER/test.txt $ORACLE_FOLDER/ref_test.amr
+    # cp $ALIGNED_FOLDER/train.txt $ORACLE_FOLDER/ref_train.amr
+    # cp $ALIGNED_FOLDER/dev.txt $ORACLE_FOLDER/ref_dev.amr
+    # cp $ALIGNED_FOLDER/test.txt $ORACLE_FOLDER/ref_test.amr
     # copy alignment probabilities (if provided)
     [ -f "$ALIGNED_FOLDER/alignment.trn.pretty" ] \
         && cp $ALIGNED_FOLDER/alignment.trn.pretty $ORACLE_FOLDER/
@@ -41,19 +41,39 @@ else
         && cp $ALIGNED_FOLDER/alignment.trn.align_dist.npy $ORACLE_FOLDER/
 
     echo -e "\nTrain data"
+if [ $MODE = "doc" ] || [ $MODE = "doc+sen" ];then
+        python transition_amr_parser/get_doc_amr_from_sen.py \
+            --in-amr $AMR_TRAIN_FILE \
+            --coref-fof $TRAIN_COREF \
+            --fof-path $FOF_PATH \
+            --norm $NORM \
+            --out-amr $ORACLE_FOLDER/train_${NORM}.docamr 
+            
+        TRAIN_IN_AMR=$ORACLE_FOLDER/train_${NORM}.docamr
+        if [ $MODE = "doc+sen" ];then
+            echo -e "\n Adding sentence data"
+            python transition_amr_parser/add_sentence_amrs_to_file.py \
+                --in-amr $AMR_SENT_TRAIN_FILE \
+                --out-amr $TRAIN_IN_AMR 
+        fi
+    
+    fi
 
-    python transition_amr_parser/amr_machine_docamr.py \
-        --in-aligned-amr $AMR_TRAIN_FILE \
+    if [ $MODE = "sen" ];then
+        echo -e "\n Using train data" 
+        TRAIN_IN_AMR=$AMR_TRAIN_FILE
+        cp $TRAIN_IN_AMR $ORACLE_FOLDER/ref_train.amr
+    fi
+
+    python transition_amr_parser/amr_machine.py \
+        --in-aligned-amr $TRAIN_IN_AMR \
         --out-machine-config $ORACLE_FOLDER/machine_config.json \
         --out-actions $ORACLE_FOLDER/train.actions \
         --out-tokens $ORACLE_FOLDER/train.en \
-        --absolute-stack-positions  \
+        --absolute-stack-positions \
         --out-stats-vocab $ORACLE_FOLDER/train.actions.vocab \
         --use-copy ${USE_COPY} \
-        --coref-fof DATA/AMR3.0/coref/train_coref.fof \
-        --norm no-merge \
-        --fof-path /dccstor/ykt-parse/SHARED/CORPORA/AMR/amr_annotation_3.0/
-
+       
         
         # --reduce-nodes all
 
@@ -65,31 +85,74 @@ else
     done
 
     echo -e "\nDev data"
+    if [ $MODE = "doc" ];then
+        echo -e "\n Making docamr dev data"
+        python transition_amr_parser/get_doc_amr_from_sen.py \
+            --in-amr $AMR_DEV_FILE \
+            --coref-fof $DEV_COREF \
+            --fof-path $FOF_PATH \
+            --norm $NORM \
+            --out-amr $ORACLE_FOLDER/dev_${NORM}.docamr
+        DEV_IN_AMR=$ORACLE_FOLDER/dev_${NORM}.docamr
+    fi
 
-    python transition_amr_parser/amr_machine_docamr.py \
-        --in-aligned-amr $AMR_DEV_FILE \
+    
+    if [ $MODE = "doc+sen" ];then
+        echo -e "\n Using sentence dev data"
+        DEV_IN_AMR=$AMR_SENT_DEV_FILE
+    fi
+
+    if [ $MODE = "sen" ];then
+        echo -e "\n Using dev data" 
+        DEV_IN_AMR=$AMR_DEV_FILE
+        cp $DEV_IN_AMR $ORACLE_FOLDER/ref_dev.amr
+    fi
+
+
+
+    
+    
+    python transition_amr_parser/amr_machine.py \
+        --in-aligned-amr $DEV_IN_AMR \
         --out-machine-config $ORACLE_FOLDER/machine_config.json \
         --out-actions $ORACLE_FOLDER/dev.actions \
         --out-tokens $ORACLE_FOLDER/dev.en \
         --absolute-stack-positions  \
         --use-copy ${USE_COPY} \
-        --coref-fof DATA/AMR3.0/coref/dev1_coref.fof \
-        --norm no-merge \
-        --fof-path /dccstor/ykt-parse/SHARED/CORPORA/AMR/amr_annotation_3.0/
         # --reduce-nodes all
 
     echo -e "\nTest data"
 
-    python transition_amr_parser/amr_machine_docamr.py \
-        --in-aligned-amr $AMR_TEST_FILE \
+    if [ $MODE = "doc" ];then
+        echo -e "\n Making docamr test data"
+        python transition_amr_parser/get_doc_amr_from_sen.py \ 
+            --in-amr $AMR_TEST_FILE \
+            --coref-fof $DEV_COREF  \
+            --fof-path $FOF_PATH  \
+            --norm $NORM \
+            --out-amr $ORACLE_FOLDER/test_${NORM}.docamr
+
+        TEST_IN_AMR=$ORACLE_FOLDER/test_${NORM}.docamr
+    fi
+    
+    if [ $MODE = "doc+sen" ];then
+        echo -e "\n Using sentence test data"
+        TEST_IN_AMR=$AMR_SENT_TEST_FILE
+    fi
+
+    if [ $MODE = "sen" ];then
+        echo -e "\n Using test data" 
+        TEST_IN_AMR=$AMR_TEST_FILE
+        cp $TEST_IN_AMR $ORACLE_FOLDER/ref_test.amr
+    fi
+
+    python transition_amr_parser/amr_machine.py \
+        --in-aligned-amr $TEST_IN_AMR \
         --out-machine-config $ORACLE_FOLDER/machine_config.json \
         --out-actions $ORACLE_FOLDER/test.actions \
         --out-tokens $ORACLE_FOLDER/test.en \
         --absolute-stack-positions  \
         --use-copy ${USE_COPY} \
-        --coref-fof DATA/AMR3.0/coref/test_coref.fof \
-        --norm no-merge \
-        --fof-path /dccstor/ykt-parse/SHARED/CORPORA/AMR/amr_annotation_3.0/
         # --reduce-nodes all
 
     touch $ORACLE_FOLDER/.done
