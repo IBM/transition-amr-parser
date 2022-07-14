05a1501a71e363bb09f1f79fc629722dd00e8c58
sadhana01 <sadhana_kumaravel@yahoo.co.in>
2022-07-14 12:48:54 -0400

    add preprocessing of docamr files

--- tests/oracles/amr_o10_doc.sh
+++ tests/oracles/amr_o10_doc.sh
@@ -7,18 +7,23 @@ set -o nounset
 
 oracle_folder=DATA/AMR3.0/oracles/o10_pinitos_doc_test_v0.2/
 mkdir -p $oracle_folder 
- 
+
+#make_doc_amr
+python transition_amr_parser/get_doc_amr_from_sen.py \
+            --in-amr $gold_amr \
+            --coref-fof DATA/AMR3.0/coref/train_coref.fof \
+            --fof-path <path to AMR3.0> \
+            --norm $NORM \
+            --out-amr $oracle_folder/train_${NORM}.docamr
+
 # get actions from oracle
 python transition_amr_parser/amr_machine.py \
-    --in-aligned-amr $gold_amr \
+    --in-aligned-amr $oracle_folder/train_${NORM}.docamr \
     --out-machine-config $oracle_folder/machine_config.json \
     --out-actions $oracle_folder/train.actions \
     --out-tokens $oracle_folder/train.tokens \
     --use-copy 1 \
-    --absolute-stack-positions \
-    --coref-fof DATA/AMR3.0/coref/train_coref.fof \
-    --norm no-merge \
-    --fof-path <path to AMR3.0> 
+    --absolute-stack-positions
     # --reduce-nodes all
 
 # play actions on state machine
