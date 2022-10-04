de44dda1b60041b5f7594da12be3fee2f64331bf
tahira <tahira.naseem@gmail.com>
2022-10-04 00:25:21 -0400

    remove docAMR call from test_sliding.sh

--- run/test_sliding.sh
+++ run/test_sliding.sh
@@ -205,7 +205,7 @@ python transition_amr_parser/amr_machine.py \
 if [ $MODE == "doc" ] || [ $MODE == "doc+sen+ft" ];then
     echo "mode doc"
     echo -e "\n Changing rep of dev/test data to docAMR "
-    python docAMR/doc_amr.py \
+    python transition_amr_parser/doc_amr.py \
         --in-doc-amr-pairwise ${results_prefix}.amr \
         --rep docAMR \
 	--pairwise-coref-rel :same-as \
@@ -215,7 +215,7 @@ elif [ $MODE == "doc+sen" ];then
     echo "mode doc+sen"
     if [ $data_split2 == "test" ]; then
         echo -e "\n Changing rep of test data(doc dev) to docAMR "
-        python docAMR/doc_amr.py \
+        python transition_amr_parser/doc_amr.py \
             --in-doc-amr-pairwise ${results_prefix}.amr \
             --rep docAMR \
 	    --pairwise-coref-rel :same-as \
