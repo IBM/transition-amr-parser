c9bf86bd300eefa23b5a31912dc1365b59cda094
ramon-astudillo <ramon@astudillo.com>
2022-11-01 11:04:00 -0400

    Remove debug mode by default

--- transition_amr_parser/__init__.py
+++ transition_amr_parser/__init__.py
@@ -1,9 +1,13 @@
-import sys
-import ipdb
-import traceback
+# set this to true to start the debugger on any exception
+DEBUG_MODE = False
+if DEBUG_MODE:
+    import sys
+    import ipdb
+    import traceback
 
-def debughook(etype, value, tb):
-    traceback.print_exception(etype, value, tb)
-    print()
-    ipdb.pm() # post-mortem debugger
-sys.excepthook = debughook
+    def debughook(etype, value, tb):
+        traceback.print_exception(etype, value, tb)
+        print()
+        # post-mortem debugger
+        ipdb.pm()
+    sys.excepthook = debughook
