7e19cd8e24e9963bd7dabd867c7870a6c8c6e5de
ramon-astudillo <ramon@astudillo.com>
2022-11-17 07:11:38 -0500

    TLP downloader OSX compatible

--- tests/download_little_prince.sh
+++ tests/download_little_prince.sh
@@ -2,22 +2,25 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-[ -d DATA/LP/ ] && rm -R DATA/LP
+# [ -d DATA/LP/ ] && rm -R DATA/LP
 mkdir -p DATA/LP/corpora/
 
 # Download data
 
 if [ ! -f DATA/LP/corpora/dev.txt ];then
-    wget -O DATA/LP/corpora/dev.txt https://amr.isi.edu/download/amr-bank-struct-v1.6-dev.txt 
-    sed '1,2d' -i DATA/LP/corpora/dev.txt    
+    wget --no-check-certificate -O DATA/LP/corpora/dev.txt.tmp https://amr.isi.edu/download/amr-bank-struct-v1.6-dev.txt 
+    sed '1,2d' DATA/LP/corpora/dev.txt.tmp > DATA/LP/corpora/dev.txt    
+    rm DATA/LP/corpora/dev.txt.tmp
 fi
 
 if [ ! -f DATA/LP/corpora/train.txt ];then
-    wget -O DATA/LP/corpora/train.txt https://amr.isi.edu/download/amr-bank-struct-v1.6-training.txt 
-    sed '1,2d' -i DATA/LP/corpora/train.txt    
+    wget --no-check-certificate -O DATA/LP/corpora/train.txt.tmp https://amr.isi.edu/download/amr-bank-struct-v1.6-training.txt 
+    sed '1,2d' DATA/LP/corpora/train.txt.tmp > DATA/LP/corpora/train.txt
+    rm DATA/LP/corpora/train.txt.tmp
 fi    
 
 if [ ! -f DATA/LP/corpora/test.txt ];then
-    wget -O DATA/LP/corpora/test.txt https://amr.isi.edu/download/amr-bank-struct-v1.6-test.txt 
-    sed '1,2d' -i DATA/LP/corpora/test.txt    
+    wget --no-check-certificate -O DATA/LP/corpora/test.txt.tmp https://amr.isi.edu/download/amr-bank-struct-v1.6-test.txt 
+    sed '1,2d' DATA/LP/corpora/test.txt.tmp > DATA/LP/corpora/test.txt 
+    rm DATA/LP/corpora/test.txt.tmp
 fi
