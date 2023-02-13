17654ab1764a58932e391714e02393b3d9cddab9
GX-Xu <gx.xu@ibm.com>
2023-02-13 11:55:51 -0500

    pypi distribution scripts update

--- neural_parser/__init__.py
+++
@@ -1,4 +0,0 @@
-# import neural_parser.fairseq_ext as fairseq_ext
-# import neural_parser.transition_amr_parser as transition_amr_parser
-from . import fairseq_ext
-from . import transition_amr_parser
--- neural_parser/fairseq_ext/__init__.py
+++
@@ -1,7 +0,0 @@
-# to register all the user defined modules to fairseq
-# import neural_parser.fairseq_ext.criterions    # noqa
-# import neural_parser.fairseq_ext.models    # noqa
-# import neural_parser.fairseq_ext.tasks    # noqa
-from . import criterions
-from . import models
-from . import tasks
