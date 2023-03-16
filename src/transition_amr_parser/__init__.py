231cfe3c7afd8b948720a666f6e73b44442592a0
GX-Xu <gx.xu@ibm.com>
2023-03-16 09:26:27 -0400

    check for torch_scatter

--- src/transition_amr_parser/__init__.py
+++ src/transition_amr_parser/__init__.py
@@ -1,3 +1,14 @@
+import subprocess
+import warnings
+# check for installation of torch-scatter
+try:
+    import torch_scatter
+except:
+    warnings.warn("torch-scatter is either not installed or not properly installed; please check for the appropriate version", UserWarning)
+    cmd = ["pip", "install", "torch-scatter", "-f", "https://data.pyg.org/whl/torch-1.13.1+cu117.html"]
+    print("try downloading torch-scatter")
+    subprocess.call(cmd)
+
 # set this to true to start the debugger on any exception
 DEBUG_MODE = False
 if DEBUG_MODE:
